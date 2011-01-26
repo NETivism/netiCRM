e6688b89cc602498221c93db2b453fc0df659e98
Jimmy <jimmy@netivism.com.tw>
2011-01-26 16:11:38 +0800

    Process bar and default style.

--- drupal/modules/civicrm_pcplist/civicrm_pcplist_item.tpl.php
+++ drupal/modules/civicrm_pcplist/civicrm_pcplist_item.tpl.php
@@ -1,8 +1,16 @@
-<div class="pcplist-item pcplist-item-<?php print $pcp_id ?>">
+<div class="pcplist-item pcplist-item-<?php print $pcp_id ?> clear-block">
   <div class="pcplist-title"><a href="<?php print $view_url ?>"><?php print $title ?></a></div>
+  <div class="pcplist-achievement">
+    <div class="pcplist-goal"><?php print t('Goal') ?>: <?php print $goal_amount ?></div>
+    <div class="pcplist-fill-wrapper">
+      <div class="phplist-fill-height" style="height:<?php print $achieved_percentage ?>">
+        <div class="phplist-fill-pointer"><?php print $achieved_percentage ?></div>
+      </div>
+    </div> 
+    <div class="pcplist-achieved"><?php print t('Achieved') ?>: <?php print $achieved_amount ?></div>
+  </div>
   <div class="pcplist-image"><img src="<?php print $image_src ?>" /></div>
   <div class="pcplist-count"><label><?php print t("Contribution count") ?>:</label> <?php print $contribution_count ?></div>
-  <div class="pcplist-intro"><?php print $intro_text ?>  <span class="more"><a href="<?php print $view_url ?>"><?php print t('more') ?></a></span></div>
+  <div class="pcplist-intro"><?php print $intro_text ?>  <span class="more">&raquo; <a href="<?php print $view_url ?>"><?php print t('more') ?></a></span></div>
   <div class="pcplist-contrib"><?php print $contribution_link ?></div>
-  <div class="pcplist-achievement"><label><?php print t('Achieved contribution') ?>:</label> <?php print $achieved_amount ?>(<?php print t('Achieved') ?>) / <?php print $goal_amount ?>(<?php print t('goal') ?>)</div>
 </div>
