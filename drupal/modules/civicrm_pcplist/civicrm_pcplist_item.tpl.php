8dbd633581e8c757d0180e46a47d11dff239d1e7
Jimmy <jimmy@netivism.com.tw>
2011-01-29 22:52:46 +0800

    Enhance of pcplist.

--- drupal/modules/civicrm_pcplist/civicrm_pcplist_item.tpl.php
+++ drupal/modules/civicrm_pcplist/civicrm_pcplist_item.tpl.php
@@ -3,14 +3,14 @@
   <div class="pcplist-achievement">
     <div class="pcplist-goal"><?php print t('Goal') ?>: <?php print $goal_amount ?></div>
     <div class="pcplist-fill-wrapper">
-      <div class="phplist-fill-height" style="height:<?php print $achieved_percentage ?>">
-        <div class="phplist-fill-pointer"><?php print $achieved_percentage ?></div>
+      <div class="pcplist-fill-height" style="width:<?php print $achieved_percentage ?>">
+        <div class="pcplist-fill-pointer"><?php print $achieved_percentage ?></div>
       </div>
     </div> 
     <div class="pcplist-achieved"><?php print t('Achieved') ?>: <?php print $achieved_amount ?></div>
+    <div class="pcplist-contrib">&raquo; <?php print $contribution_link ?><span></span></div>
   </div>
   <div class="pcplist-image"><img src="<?php print $image_src ?>" /></div>
   <div class="pcplist-count"><label><?php print t("Contribution count") ?>:</label> <?php print $contribution_count ?></div>
-  <div class="pcplist-intro"><?php print $intro_text ?>  <span class="more">&raquo; <a href="<?php print $view_url ?>"><?php print t('more') ?></a></span></div>
-  <div class="pcplist-contrib"><?php print $contribution_link ?></div>
+  <div class="pcplist-intro"><?php print $intro_text ?>  <span class="more">&raquo; <a href="<?php print $view_url ?>"><?php print t('More') ?></a></span></div>
 </div>
