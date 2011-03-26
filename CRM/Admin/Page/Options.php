095f90cb390d16af5206c93225e76dc82db6646e
Jimmy Huang <jimmy@netivism.com.tw>
2011-03-26 21:49:15 +0800

    Fixes #2621 .

--- CRM/Admin/Page/Options.php
+++ CRM/Admin/Page/Options.php
@@ -109,7 +109,7 @@ class CRM_Admin_Page_Options extends CRM_Core_Page_Basic
                                                                          'reset=1' )) );
             CRM_Utils_System::appendBreadCrumb( $breadCrumb ); 
        } else {
-            CRM_Utils_System::setTitle(ts('%1 Options', array(1 => self::$_GName)));
+            CRM_Utils_System::setTitle(ts('%1 Options', array(1 => ts(self::$_GName))));
         }
         if ( in_array( self::$_gName, array('from_email_address', 'email_greeting', 'postal_greeting', 'addressee') ) ) {
             $this->assign( 'showIsDefault', true );
