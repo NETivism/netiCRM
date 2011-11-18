c4a083c5569e7cbffceda9517a5531d6c9128a46
Jimmy <jimmy@netivism.com.tw>
2011-11-18 17:45:58 +0800

    Add donation user list functions

---
+++ drupal/modules/civicrm_contribution_cck/civicrm_contribution_cck_donors.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @donors
+ * @node
+ */
+?>
+<div class="civicc-donation civicc-type-<?php print $delta; ?>">
+  <h3><?php print $type; ?></h3>
+  <div class=donors>
+  <?php print $content; ?>
+  </div>
+</div>
