5872ed1a308816c8665ea6a7d1db2328e1bae30c
Jimmy Huang <jimmy@netivism.com.tw>
2012-04-02 18:06:56 +0800

    fixes #6238

--- CRM/Event/Selector/Search.php
+++ CRM/Event/Selector/Search.php
@@ -455,6 +455,11 @@ class CRM_Event_Selector_Search extends CRM_Core_Selector_Base implements CRM_Co
                 $pre = array( 
                              array('desc' => ts('Contact Type') ), 
                              array( 
+                                   'name'      => '#', 
+                                   'sort'      => 'participant_id', 
+                                   'direction' => CRM_Utils_Sort::DONTCARE,
+                                   ),
+                             array( 
                                    'name'      => ts('Participant'), 
                                    'sort'      => 'sort_name', 
                                    'direction' => CRM_Utils_Sort::DONTCARE,
