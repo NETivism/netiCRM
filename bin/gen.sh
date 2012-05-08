1a9db0248ab170a31dda5ca7329ce5747349c59d
Jimmy Huang <jimmy@netivism.com.tw>
2012-05-09 02:44:22 +0800

    Automatic generate mo file to correct path

--- bin/gen.sh
+++ bin/gen.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 CALLEDPATH=`dirname $0`
+LANGUAGE='zh_TW'
+if [ -d $CALLEDPATH/../l10n/$LANGUAGE ]; then
+  if [ ! -d $CALLEDPATH/../l10n/$LANGUAGE/LC_MESSAGES ]; then
+    mkdir $CALLEDPATH/../l10n/$LANGUAGE/LC_MESSAGES
+  fi
+  msgfmt $CALLEDPATH/../l10n/$LANGUAGE/civicrm.po -o $CALLEDPATH/../l10n/$LANGUAGE/LC_MESSAGES/civicrm.mo
+fi
+
 if [ -d $CALLEDPATH/../xml ]; then
   cd $CALLEDPATH/../xml
   php GenCode.php 
 fi
+
