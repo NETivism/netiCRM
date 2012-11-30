4cd9c6a92c7f4c41cad6861e28dd7e633a12b8ad
Jimmy Huang <jimmy@netivism.com.tw>
2012-11-30 12:57:53 +0800

    fixes #8152 translation.

--- CRM/Contact/Form/Search/Criteria.php
+++ CRM/Contact/Form/Search/Criteria.php
@@ -168,7 +168,7 @@ class CRM_Contact_Form_Search_Criteria {
 
         // add the option to display relationships
         $rTypes = CRM_Core_PseudoConstant::relationshipType();
-        $rSelect = array('' => ts('- Select Relationship Type-'));
+        $rSelect = array('' => ts('- Select Relationship Type -'));
         foreach ($rTypes as $rid => $rValue) {
           if ($rValue['label_a_b'] == $rValue['label_b_a']) {
             $rSelect[$rid] = $rValue['label_a_b'];
