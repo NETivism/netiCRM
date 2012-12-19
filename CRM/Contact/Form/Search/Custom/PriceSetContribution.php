99c71ccba703d2561574936132e3d33ce3c92b19
Jimmy Huang <jimmy@netivism.com.tw>
2012-12-19 18:04:32 +0800

    Add address and phone / email for contribution priceset custom search

--- CRM/Contact/Form/Search/Custom/PriceSetContribution.php
+++ CRM/Contact/Form/Search/Custom/PriceSetContribution.php
@@ -44,36 +44,30 @@ class CRM_Contact_Form_Search_Custom_PriceSetContribution
 
     protected $_tableName = null;
 
+
     function __construct( &$formValues ) {
         parent::__construct( $formValues );
+          $this->_price_set_id= CRM_Utils_Array::value( 'price_set_id', $this->_formValues );
+          $this->setColumns( );
 
-        $this->_price_set_id= CRM_Utils_Array::value( 'price_set_id', $this->_formValues );
-
-        $this->setColumns( );
-
-        if ( $this->_price_set_id ) {
-            $this->buildTempTable( );
-        
-            $this->fillTable( );
-        }
-
+          if ( $this->_price_set_id ) {
+              $this->buildTempTable( );
+              $this->fillTable( );
+          }
     }
 
     function __destruct( ) {
-        /*
         if ( $this->_eventID ) {
             $sql = "DROP TEMPORARY TABLE {$this->_tableName}";
-            CRM_Core_DAO::executeQuery( $sql,
-                                        CRM_Core_DAO::$_nullArray ) ;
+            CRM_Core_DAO::executeQuery( $sql, CRM_Core_DAO::$_nullArray ) ;
         }
-        */
     }
 
     function buildTempTable( ) {
-        $randomNum = md5( uniqid( ) );
+        $randomNum = md5($_GET['qfKey'].$this->_price_set_id);
         $this->_tableName = "civicrm_temp_custom_{$randomNum}";
         $sql = "
-CREATE TEMPORARY TABLE {$this->_tableName} (
+CREATE TEMPORARY TABLE IF NOT EXISTS {$this->_tableName} (
   id int unsigned NOT NULL AUTO_INCREMENT,
   entity_table varchar(64) NOT NULL,
   entity_id int unsigned NOT NULL,
@@ -84,19 +78,21 @@ CREATE TEMPORARY TABLE {$this->_tableName} (
                                               'entity_id') ) ) {
                 continue;
             }
-            $sql .= "{$fieldName} int default 0,\n";
+            $sql .= "{$fieldName} varchar(32) default '',\n";
         }
         
         $sql .= "
 PRIMARY KEY ( id ),
 UNIQUE INDEX unique_entity ( entity_table, entity_id )
-) ENGINE=HEAP
+) ENGINE=HEAP DEFAULT CHARSET=utf8
 ";
         
         CRM_Core_DAO::executeQuery( $sql, CRM_Core_DAO::$_nullArray ) ;
     }
 
     function fillTable( ) {
+        static $filled;
+        if($filled) return;
         $sql = "
 SELECT l.price_field_value_id as price_field_value_id, 
        l.qty,
@@ -129,11 +125,38 @@ ORDER BY l.entity_table, l.entity_id ASC
         }
         $dao = CRM_Core_DAO::executeQuery("SELECT * FROM $this->_tableName");
         while($dao->fetch()){
-            $sql = "SELECT contact_id FROM $dao->entity_table WHERE id = $dao->entity_id";
-            $contact_id = CRM_Core_DAO::singleValueQuery($sql);
-            $sql = "UPDATE {$this->_tableName} SET contact_id = $contact_id WHERE entity_id = $dao->entity_id AND entity_table = '$dao->entity_table'";
-            CRM_Core_DAO::executeQuery( $sql, CRM_Core_DAO::$_nullArray );
+            // contact id and total amount
+            $sql = "SELECT contact_id, total_amount FROM $dao->entity_table WHERE id = $dao->entity_id";
+            $data = CRM_Core_DAO::executeQuery($sql);
+            $data->fetch();
+
+            // email
+            if($data->contact_id){
+              $sql = "SELECT email FROM civicrm_email WHERE contact_id = {$data->contact_id} ORDER BY is_primary DESC";
+              $email = CRM_Core_DAO::singleValueQuery($sql);
+
+              $sql = "SELECT phone FROM civicrm_phone WHERE contact_id = {$data->contact_id} ORDER BY is_primary DESC";
+              $phone = CRM_Core_DAO::singleValueQuery($sql);
+
+              $sql = "SELECT a.postal_code, b.name as state_province, a.city, a.street_address FROM civicrm_address a INNER JOIN civicrm_state_province b ON a.state_province_id = b.id WHERE a.contact_id = {$data->contact_id} ORDER BY a.is_primary DESC";
+              $addr = CRM_Core_DAO::executeQuery($sql);
+              $addr->fetch();
+              $addr->state_province = ts($addr->state_province);
+              
+              $sql = "UPDATE {$this->_tableName} 
+              SET contact_id = {$data->contact_id},
+              email = '{$email}',
+              phone = '{$phone}',
+              total_amount = '{$data->total_amount}',
+              zip = '{$addr->postal_code}',
+              county = '{$addr->state_province}',
+              city = '{$addr->city}',
+              address = '{$addr->street_address}'
+              WHERE entity_id = {$dao->entity_id} AND entity_table = '{$dao->entity_table}'";
+              CRM_Core_DAO::executeQuery( $sql, CRM_Core_DAO::$_nullArray );
+            }
         }
+        $filled = true;
     }
 
     function priceSetDAO( $price_set_id = null ) {
@@ -198,8 +221,17 @@ WHERE  p.price_set_id = e.id
     }
 
     function setColumns( ) {
-        $this->_columns = array( ts('Contact Id')      => 'contact_id'    ,
-                                 ts('Name')            => 'display_name'  );
+        $this->_columns = array(
+          ts('Contact Id')      => 'contact_id',
+          ts('Name')            => 'display_name',
+          ts('Postal Code')     => 'zip',
+          ts('State/Province')  => 'county',
+          ts('City')            => 'city',
+          ts('Address')         => 'address',
+          ts('Phone')           => 'phone',
+          ts('Email')           => 'email',
+          ts('Total Amount')    => 'total_amount',
+        );
 
         if ( ! $this->_price_set_id ) {
             return;
