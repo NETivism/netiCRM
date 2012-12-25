0d42b144711aa5460578394f464e7192149cce86
jimmy <jimmy@netivism.com.tw>
2012-12-25 14:23:26 +0800

    Add new payment method esunbank. #7944, #8394

---
+++ CRM/Core/Payment/ESUNBANK.php
@@ -0,0 +1,116 @@
+<?php 
+date_default_timezone_set('Asia/Taipei');
+require_once 'CRM/Core/Payment.php';
+
+class CRM_Core_Payment_Esunbank extends CRM_Core_Payment { 
+    /**
+     * mode of operation: live or test
+     *
+     * @var object
+     * @static
+     */
+    static protected $_mode = null;
+
+    /**
+     * We only need one instance of this object. So we use the singleton
+     * pattern and cache the instance in this variable
+     *
+     * @var object
+     * @static
+     */
+    static private $_singleton = null;
+
+
+    /**
+     * Constructor
+     *
+     * @param string $mode the mode of operation: live or test
+     *
+     * @return void
+     */
+    function __construct( $mode, &$paymentProcessor ) {
+      $this->_mode             = $mode;
+      $this->_paymentProcessor = $paymentProcessor;
+      $this->_processorName    = 'ESUNBANK';
+      $config =& CRM_Core_Config::singleton( );
+      $this->_config = $config;
+    }
+
+    /** 
+     * singleton function used to manage this object 
+     * 
+     * @param string $mode the mode of operation: live or test
+     *
+     * @return object 
+     * @static 
+     * 
+     */ 
+    static function &singleton( $mode, &$paymentProcessor ) {
+        $processorName = $paymentProcessor['name'];
+        if (self::$_singleton[$processorName] === null ) {
+            self::$_singleton[$processorName] = new CRM_Core_Payment_ESUNBANK( $mode, $paymentProcessor );
+        }
+        return self::$_singleton[$processorName];
+    }
+
+    /** 
+     * This function checks to see if we have the right config values 
+     * 
+     * @return string the error message if any 
+     * @public 
+     */ 
+    function checkConfig( ) {
+        $config = CRM_Core_Config::singleton( );
+
+        $error = array( );
+
+        if ( empty( $this->_paymentProcessor['user_name'] ) ) {
+            $error[] = ts( 'User Name is not set in the Administer CiviCRM &raquo; Payment Processor.' );
+        }
+        
+        if ( empty( $this->_paymentProcessor['password'] ) ) {
+            $error[] = ts( 'Password is not set in the Administer CiviCRM &raquo; Payment Processor.' );
+        }
+        
+        if ( ! empty( $error ) ) {
+            return implode( '<p>', $error );
+        } else {
+            return null;
+        }
+    }
+
+    function setExpressCheckOut( &$params ) {
+      CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+    }
+    function getExpressCheckoutDetails( $token ) {
+      CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+    }
+    function doExpressCheckout( &$params ) {
+      CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+    }
+    function doDirectPayment( &$params ) {
+      CRM_Core_Error::fatal( ts( 'This function is not implemented' ) );
+    }
+
+    /**  
+     * Sets appropriate parameters for checking out to google
+     *  
+     * @param array $params  name value pair of contribution datat
+     *  
+     * @return void  
+     * @access public 
+     *  
+     */  
+    function doTransferCheckout( &$params, $component ) {
+      $component = strtolower( $component );
+      if ( $component != 'contribute' && $component != 'event' ) {
+        CRM_Core_Error::fatal( ts('Component is invalid') );
+      }
+      if(module_load_include('inc', 'civicrm_esunbank', 'civicrm_esunbank.checkout') === FALSE){
+        CRM_Core_Error::fatal( 'Module civicrm_esunbank doesn\'t exists.' );
+      }
+      else{
+        civicrm_esunbank_do_transfer_checkout($params, $component, $this->_paymentProcessor);
+      }
+    }
+}
