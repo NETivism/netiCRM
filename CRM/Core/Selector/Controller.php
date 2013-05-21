05be28496768b22d2cf942a5bba250eb9949f4af
Jimmy Huang <jimmy@netivism.com.tw>
2013-05-22 00:58:27 +0800

    Add column header hook for alternate search result.

--- CRM/Core/Selector/Controller.php
+++ CRM/Core/Selector/Controller.php
@@ -305,6 +305,7 @@ class CRM_Core_Selector_Controller {
         if ($this->_output == self::EXPORT || $this->_output == self::SCREEN ) {
             // get rows (without paging criteria)
             $rows = self::getRows( $this );
+            CRM_Utils_Hook::searchColumns( $contextName, $columnHeaders, $rows, $this );
             if ( $this->_output == self::EXPORT ) {
                 // export the rows.
                 CRM_Core_Report_Excel::writeCSVFile( $this->_object->getExportFileName( ),
@@ -320,6 +321,7 @@ class CRM_Core_Selector_Controller {
         } else {
             // output requires paging/sorting capability
             $rows = self::getRows( $this );
+            CRM_Utils_Hook::searchColumns( $contextName, $columnHeaders, $rows, $this );
             $rowsEmpty = count( $rows ) ? false : true;
             $qill      = $this->getQill( );
             $summary   = $this->getSummary( );
