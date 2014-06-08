1750a9b696b9cb451c4c0773caa03ff5dfd90304
Jimmy Huang <jimmy@netivism.com.tw>
2014-06-08 19:19:24 +0800

    refs #12458 , disable submit should work only qfkey exist.

--- js/jquery/jquery.disablesubmit.js
+++ js/jquery/jquery.disablesubmit.js
@@ -3,7 +3,7 @@
     var qfkey = getUrlParams('qfKey');
     var submitted = getCookie(qfkey);
     var $obj = $('input[onclick*=submitOnce]')
-    if($obj.length){
+    if($obj.length && qfkey){
       if(submitted == "1"){
         $obj.each(function(){
           $(this)[0].onclick = null;
