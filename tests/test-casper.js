c20934d89f9def6b0e81be86a1cfee39356677c4
Jimmy Huang <jimmy@netivism.com.tw>
2014-10-07 20:42:40 +0800

    Change casperjs to new test framework.

--- tests/test-casper.js
+++ tests/test-casper.js
@@ -3,39 +3,18 @@
  *
  * read more at http://casperjs.org/
  */
-function run(){
-  var title = this.getTitle();
-  casper.log("Testing... "+title, 'info');
-  if(this.exists('.error-ci')){
-    var error = this.getHTML('.error-ci');
-    casper.log(error, 'error');
-  }
+function Cow() {
+  this.mowed = false;
+  this.moo = function moo() {
+      this.mowed = true; // mootable state: don't do that at home
+      return 'moo!';
+  };
 }
-var casper = require('casper').create({
-  verbose: true,
-  logLevel: 'debug'
+
+// cow-test.js
+casper.test.begin('Cow can moo', 2, function suite(test) {
+    var cow = new Cow();
+    test.assertEquals(cow.moo(), 'moo!');
+    test.assert(cow.mowed);
+    test.done();
 });
-var base_url = 'http://127.0.0.1:8080/';
-
-// login for test
-casper.start(base_url, function() {
-  casper.log("Start casper debug", 'debug');
-  this.fill('#user-login-form', {
-    'name':'admin',
-    'pass':'123456'
-  }, true);
-});
-
-// run pages
-// add individual
-casper.thenOpen(base_url+'civicrm/contact/add&reset=1&ct=Individual', run);
-
-// contribute
-casper.thenOpen(base_url+'civicrm/contribute/add&reset=1&action=add&context=standalone', run);
-
-// event
-casper.thenOpen(base_url+'civicrm/event&reset=1', run);
-casper.thenOpen(base_url+'civicrm/participant/add&reset=1&action=add&context=standalone', run);
-
-casper.run();
-casper.exit();
