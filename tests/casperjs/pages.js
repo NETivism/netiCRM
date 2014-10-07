8f7e027631e9b6b1ecb45f835441adf01275bf4b
Jimmy Huang <jimmy@netivism.com.tw>
2014-10-07 23:48:55 +0800

    Trying to fix page test.

--- tests/casperjs/pages.js
+++ tests/casperjs/pages.js
@@ -1,5 +1,6 @@
 /**
  * This is a screen test script based on casper.js
+ * The variable using .travis.yml on root of netiCRM
  *
  * read more at http://casperjs.org/
  */
@@ -7,10 +8,10 @@
 // cow-test.js
 var base_url = 'http://127.0.0.1:8080/';
 var site_name = 'netiCRM';
-casper.test.begin('Pages running test', 2, function suite(test) {
+casper.test.begin('Page output correct test', 2, function suite(test) {
   this.runpage = function(title){
     var full_title = title + ' | ' + site_name;
-    test.assertTitle(full_title, title+' matched');
+    test.assertTitle(full_title, 'Page title should be matched: ' + title);
     test.assertDoesntExist('.error-ci');
   }
   casper.start(base_url, function() {
@@ -21,7 +22,7 @@ casper.test.begin('Pages running test', 2, function suite(test) {
     }, true);
   });
 
-  casper.thenOpen(base_url+'civicrm/contact/add?reset=1&ct=Individual', this.runpage('New 個人'));
+  casper.thenOpen(base_url+'civicrm/contact/add&reset=1&ct=Individual', this.runpage('New 個人'));
 
   casper.run(function(){
     test.done();
