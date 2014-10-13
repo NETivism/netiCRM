baa914a1e3c96bebd634ab405fd2a4e2d3e81368
Jimmy Huang <jimmy@netivism.com.tw>
2014-10-13 14:29:53 +0800

    test casper again.

--- tests/casperjs/pages.js
+++ tests/casperjs/pages.js
@@ -5,14 +5,20 @@
  * read more at http://casperjs.org/
  */
 
-// cow-test.js
 var base_url = 'http://127.0.0.1:8080/';
 var site_name = 'netiCRM';
 var url = [
  {title:'New 個人', url:'civicrm/contact/add?reset=1&ct=Individual'},
  {title:'Custom Data', url:'civicrm/admin/custom/group?reset=1'},
- {title:'Activities', url:'civicrm/activity/add?atype=3&action=add&reset=1&context=standalone'}
+ {title:'Activities', url:'civicrm/activity/add?atype=3&action=add&reset=1'}
 ];
+var lookup_title = function(u){
+  for(var i in url){
+    if(url[i].url == u){
+      return url[i].title;
+    }
+  }
+}
 casper.test.begin('Page output correct test', url.length*2+1, function suite(test) {
   casper.start(base_url, function() {
     test.assertExists('#user-login-form', "Found login form");
@@ -21,13 +27,14 @@ casper.test.begin('Page output correct test', url.length*2+1, function suite(tes
       'pass':'123456'
     }, true);
     for(var i in url){
-      var options = {'method':'get'};
-      casper.thenOpen(base_url+url[i].url, options, function(obj){
-        console.log(obj);
-        var title = '';
-        var full_title = title + ' | ' + site_name;
-        test.assertTitle(full_title, title + ' should match page title');
-        test.assertDoesntExist('.error-ci', title + ' page have no error');
+      casper.thenOpen(base_url+url[i].url, function(obj){
+        if(obj.url){
+          var url = obj.url.replace(base_url, '');
+          var title = lookup_title(url);
+          var full_title = title + ' | ' + site_name;
+          test.assertTitle(full_title, title + ' should match page title');
+          test.assertDoesntExist('.error-ci', title + ' page have no error');
+        }
       });
     }
   });
