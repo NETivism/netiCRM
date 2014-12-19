bcd9abdde22e1cec717273a2cad72f76769f3cf1
Jimmy Huang <jimmy@netivism.com.tw>
2014-12-19 18:41:02 +0800

    fixes NEGOTIATION constant notice error.

--- CRM/Utils/System/Drupal.php
+++ CRM/Utils/System/Drupal.php
@@ -491,61 +491,66 @@ class CRM_Utils_System_Drupal {
   }
 
   function languageNegotiationURL($url, $addLanguagePart = TRUE, $removeLanguagePart = FALSE) {
+    static $exists;
     if (empty($url)) {
       return $url;
     }
 
     //CRM-7803 -from d7 onward.
     $config = CRM_Core_Config::singleton();
-    if (function_exists('language_negotiation_get')) {
-      global $language;
+    $version = substr($config->userSystem->version, 0, strpos($config->userSystem->version, '.'));
+    if ($version == '7') {
+      if($exists || function_exists('language_negotiation_get')){
+        $exists = TRUE;
+        global $language;
 
-      //does user configuration allow language
-      //support from the URL (Path prefix or domain)
-      if (language_negotiation_get('language') == 'locale-url') {
-        $urlType = variable_get('locale_language_negotiation_url_part');
+        //does user configuration allow language
+        //support from the URL (Path prefix or domain)
+        if (language_negotiation_get('language') == 'locale-url') {
+          $urlType = variable_get('locale_language_negotiation_url_part');
 
-        //url prefix
-        if ($urlType == LOCALE_LANGUAGE_NEGOTIATION_URL_PREFIX) {
-          if (isset($language->prefix) && $language->prefix) {
-            if ($addLanguagePart) {
-              $url .= $language->prefix . '/';
-            }
-            if ($removeLanguagePart) {
-              $url = str_replace("/{$language->prefix}/", '/', $url);
+          //url prefix
+          if ($urlType == LOCALE_LANGUAGE_NEGOTIATION_URL_PREFIX) {
+            if (isset($language->prefix) && $language->prefix) {
+              if ($addLanguagePart) {
+                $url .= $language->prefix . '/';
+              }
+              if ($removeLanguagePart) {
+                $url = str_replace("/{$language->prefix}/", '/', $url);
+              }
             }
           }
-        }
-        //domain
-        if ($urlType == LOCALE_LANGUAGE_NEGOTIATION_URL_DOMAIN) {
-          if (isset($language->domain) && $language->domain) {
-            if ($addLanguagePart) {
-              $cleanedUrl = preg_replace('#^https?://#', '', $language->domain);
-              // drupal function base_path() adds a "/" to the beginning and end of the returned path
-              if (substr($cleanedUrl, -1) == '/') {
-                $cleanedUrl = substr($cleanedUrl, 0, -1);
+          //domain
+          if ($urlType == LOCALE_LANGUAGE_NEGOTIATION_URL_DOMAIN) {
+            if (isset($language->domain) && $language->domain) {
+              if ($addLanguagePart) {
+                $cleanedUrl = preg_replace('#^https?://#', '', $language->domain);
+                // drupal function base_path() adds a "/" to the beginning and end of the returned path
+                if (substr($cleanedUrl, -1) == '/') {
+                  $cleanedUrl = substr($cleanedUrl, 0, -1);
+                }
+                $url = (CRM_Utils_System::isSSL() ? 'https' : 'http') . '://' . $cleanedUrl . base_path();
               }
-              $url = (CRM_Utils_System::isSSL() ? 'https' : 'http') . '://' . $cleanedUrl . base_path();
-            }
-            if ($removeLanguagePart && defined('CIVICRM_UF_BASEURL')) {
-              $url = str_replace('\\', '/', $url);
-              $parseUrl = parse_url($url);
+              if ($removeLanguagePart && defined('CIVICRM_UF_BASEURL')) {
+                $url = str_replace('\\', '/', $url);
+                $parseUrl = parse_url($url);
 
-              //kinda hackish but not sure how to do it right
-              //hope http_build_url() will help at some point.
-              if (is_array($parseUrl) && !empty($parseUrl)) {
-                $urlParts           = explode('/', $url);
-                $hostKey            = array_search($parseUrl['host'], $urlParts);
-                $ufUrlParts         = parse_url(CIVICRM_UF_BASEURL);
-                $urlParts[$hostKey] = $ufUrlParts['host'];
-                $url                = implode('/', $urlParts);
+                //kinda hackish but not sure how to do it right
+                //hope http_build_url() will help at some point.
+                if (is_array($parseUrl) && !empty($parseUrl)) {
+                  $urlParts           = explode('/', $url);
+                  $hostKey            = array_search($parseUrl['host'], $urlParts);
+                  $ufUrlParts         = parse_url(CIVICRM_UF_BASEURL);
+                  $urlParts[$hostKey] = $ufUrlParts['host'];
+                  $url                = implode('/', $urlParts);
+                }
               }
             }
           }
         }
       }
     }
-    else{
+    elseif($version == '6'){
       //upto d6 only, already we have code in place for d7
       $config = CRM_Core_Config::singleton();
       $url = CRM_Utils_System_Drupal6::languageNegotiationURL($url, $addLanguagePart, $removeLanguagePart);
