8487f3593cf20a1b813e566ba9efff2005e5ccbf
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-28 21:46:43 +0800

    fixes php 5.3 compatibility

--- packages/DB/common.php
+++ packages/DB/common.php
@@ -2192,7 +2192,8 @@ class DB_common extends PEAR
      */
     function _checkManip($query)
     {
-        if ($this->_next_query_manip || (new DB)->isManip($query)) {
+        $db = new DB;
+        if ($this->_next_query_manip || $db->isManip($query)) {
             $this->_last_query_manip = true;
         } else {
             $this->_last_query_manip = false;
