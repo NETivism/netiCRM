e98da5059d4a1a096e5bdd6ad40f908275ee360c
junsuwhy <junsuwhy@gmail.com>
2015-06-16 18:10:16 +0800

    Add a tester for event 3. refs #15553

--- tests/casperjs/event_register.js
+++ tests/casperjs/event_register.js
@@ -5,10 +5,10 @@ var item = {
   url_prefix: 'http://temp.deb:8000',
   event_name_1: '測試活動',
   event_name_2: '測試有名額、不可候補',
-  event_name_3: '測試活動',
-  event_name_4: '測試活動',
-  event_name_5: '測試活動',
-  site_name: 'Drupal',
+  event_name_3: '測試有名額、可候補',
+  event_name_4: '有名額限制，需事先審核',
+  event_name_5: '無名額限制，需事先審核',
+  site_name: 'Drupal'
 }
 
 function getPageTitle(title){
@@ -52,7 +52,7 @@ casper.test.begin('Event register page test ...',7,function(test){
   casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=2', function() {
     var page_title = getPageTitle(item.event_name_2);
     test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
-    this.capture("event_register_2_0.png");
+    this.capture("picture/event_register_2_0.png");
   });
 
   casper.then(function(){
@@ -61,7 +61,7 @@ casper.test.begin('Event register page test ...',7,function(test){
     this.fill('#Register',{
       'email-5': email
     },true);
-    this.capture("event_register_2_1.png");
+    this.capture("picture/event_register_2_1.png");
     test.assertField('email-5', email);
     this.thenClick('.form-submit');
   });
@@ -69,14 +69,14 @@ casper.test.begin('Event register page test ...',7,function(test){
   casper.then(function(){
     var page_title = getPageTitle('感謝您的報名');
     test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
-    this.capture("event_register_2_2.png");
+    this.capture("picture/event_register_2_2.png");
     this.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=2');
   });
 
   casper.then(function(){
     var page_title = getPageTitle(item.event_name_2);
     test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
-    this.capture("event_register_2_3.png");
+    this.capture("picture/event_register_2_3.png");
     test.assertExists('.messages');
     test.assertSelectorHasText('.messages','此活動目前已報名額滿。');
   });
@@ -87,45 +87,56 @@ casper.test.begin('Event register page test ...',7,function(test){
 });
 
 // 測試名額限制、可候補
-/*
-casper.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=3', function() {
-    this.echo(this.getTitle());
+
+casper.test.begin('Event register page test ...',10,function(test){
+  casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=3', function() {
+    var page_title = getPageTitle(item.event_name_3);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
     this.capture("event_register_3_0.png");
-});
+  });
 
-casper.then(function(){
-  var email = 'test@ovoqnj.com';
-  this.fill('#Register',{
-    'email-5': email
-  },true);
-  this.capture("event_register_3_1.png");
-  this.echo('Fill the email : ' + email);
-  this.thenClick('.form-submit');
-});
+  casper.then(function(){
+    test.assertExists('form#Register', 'Event register page: main form is exist.');
+    var email = 'test@ovoqnj.com';
+    this.fill('#Register',{
+      'email-5': email
+    },true);
+    this.capture("picture/event_register_3_1.png");
+    test.assertField('email-5', email);
+    this.thenClick('.form-submit');
+  });
 
-casper.then(function(){
-  this.capture("event_register_3_2.png");
-  this.echo(this.getTitle());
-  this.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=3');
-});
+  casper.then(function(){
+    var page_title = getPageTitle('感謝您的報名');
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_3_2.png");
+    this.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=3');
+  });
 
-casper.then(function(){
-  this.capture("event_register_3_3.png");
-  this.echo(this.getTitle());
-  var email = 'test2@soosovk.com';
-  this.fill('#Register',{
-    'email-5': email
-  },true);
-  this.capture("event_register_3_4.png");
-  // this.echo('Fill the email : ' + email);
-  this.thenClick('.form-submit');
-  // this.echo('Message : ' + this.getHTML('.messages'));
-});
+  casper.then(function(){
+    var page_title = getPageTitle(item.event_name_3);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_3_3.png");
+    var email = 'test2@soosovk.com';
+    this.fill('#Register',{
+      'email-5': email
+    },true);
+    this.capture("picture/event_register_3_4.png");
+    test.assertField('email-5', email);
+    this.thenClick('.form-submit');
+  });
 
-casper.then(function(){
-  this.capture("event_register_3_5.png");
-  this.echo(this.getTitle());
-  this.echo('Message : ' + this.getHTML('#help p'));
+  casper.then(function(){
+    var page_title = getPageTitle('感謝您的報名');
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_3_5.png");
+    test.assertExists('#help p');
+    test.assertSelectorHasText('#help p','已為您加入到本活動的的候補名單中。');
+    test.assertSelectorHasText('#help p','若活動尚有名額，您將會收到一封電子郵件，您可以點選信件中的連結網址以完成報名程序。');
+  });
+  casper.run(function() {
+    test.done();
+  });
+
 });
-*/
 
