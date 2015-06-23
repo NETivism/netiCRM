2e011d41e6d5c534962265bdd1c50daa462af130
junsuwhy <junsuwhy@gmail.com>
2015-06-23 16:13:12 +0800

    Test registration. refs #15553

--- tests/casperjs/event_register.js
+++ tests/casperjs/event_register.js
@@ -15,7 +15,8 @@ function getPageTitle(title){
   return title + " | "+item.site_name;
 }
 
-// 1. Normal apply
+// 1. Normal registration
+
 casper.test.begin('Event register page test ...',4,function(test){
   casper.start(item.url_prefix + '/civicrm/event/register?reset=1&action=preview&id=1', function() {
     var page_title = getPageTitle(item.event_name_1);
@@ -46,7 +47,8 @@ casper.test.begin('Event register page test ...',4,function(test){
 
 });
 
-// 測試名額限制、不可候補
+
+// 2. limit participants. Not fot waiting.
 
 casper.test.begin('Event register page test ...',7,function(test){
   casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=2', function() {
@@ -86,7 +88,7 @@ casper.test.begin('Event register page test ...',7,function(test){
   });
 });
 
-// 測試名額限制、可候補
+// limit participants. Not fot waiting.
 
 casper.test.begin('Event register page test ...',10,function(test){
   casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=3', function() {
@@ -138,5 +140,121 @@ casper.test.begin('Event register page test ...',10,function(test){
     test.done();
   });
 
+});
+
+// event 4 : limit participants. Need verify.
+// Checked-1 register success
+// Checked-2 participant have get verify message.
+// Checked-3 second participant message is correct.
+
+casper.test.begin('Event register page test ...',9,function(test){
+  casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=4', function() {
+    var page_title = getPageTitle(item.event_name_4);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("event_register_4_0.png");
+  });
+
+  // First register.
+  casper.then(function(){
+    test.assertExists('form#Register', 'Event register page: main form is exist.');
+    var email = 'test@vkioob.com';
+    this.fill('#Register',{
+      'email-5': email
+    },true);
+    this.capture("picture/event_register_4_1.png");
+    test.assertField('email-5', email);
+    this.thenClick('.form-submit');
+  });
+
+  casper.then(function(){
+    var page_title = getPageTitle('感謝您的報名');
+    // Checked-1
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_4_2.png");
+    // Checked-2 
+    test.assertExists('#help p');
+    test.assertSelectorHasText('#help p','一旦您通過審核，我們將會傳一封電子郵件確認信到您的信箱，您可以點選確認信中的連結網址以完成報名程序。');
+    this.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=4');
+  });
+
+  // Second register.
+  casper.then(function(){
+    var page_title  = getPageTitle(item.event_name_4);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_4_3.png");
+    test.assertExists('.messages');
+    // Checked-3 
+    test.assertSelectorHasText('.messages','此活動目前已報名額滿。');
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+// event 5 : unlimit participants. Need verify. 
+// Checked-1 register success
+// Checked-2 participant have get verify message.
+// Checked-3 Second register success
+// Checked-4 participant have get verify message.
+
+casper.test.begin('Event register page test ...',12,function(test){
+  casper.start(item.url_prefix + '/civicrm/event/register?reset=1&id=5', function() {
+    var page_title = getPageTitle(item.event_name_5);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("event_register_5_0.png");
+  });
+
+  // First register.
+  casper.then(function(){
+    test.assertExists('form#Register', 'Event register page: main form is exist.');
+    var email = 'test@vkioob.com';
+    this.fill('#Register',{
+      'email-5': email
+    },true);
+    this.capture("picture/event_register_5_1.png");
+    test.assertField('email-5', email);
+    this.thenClick('.form-submit');
+  });
+
+  casper.then(function(){
+    var page_title = getPageTitle('感謝您的報名');
+    // Checked-1
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_5_2.png");
+    // Checked-2 
+    test.assertExists('#help p');
+    test.assertSelectorHasText('#help p','一旦您通過審核，我們將會傳一封電子郵件確認信到您的信箱，您可以點選確認信中的連結網址以完成報名程序。');
+    this.thenOpen(item.url_prefix + '/civicrm/event/register?reset=1&id=5');
+  });
+
+  // Second register.
+  casper.then(function(){
+    var page_title  = getPageTitle(item.event_name_5);
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_5_3.png");
+    test.assertExists('.messages');
+    // Checked-3 
+    var email = 'test2@xoooke.com';
+    this.fill('#Register',{
+      'email-5': email
+    },true);
+    this.capture("picture/event_register_4_4.png");
+    test.assertField('email-5', email);
+    this.thenClick('.form-submit');
+    
+  });
+
+  casper.then(function(){
+    var page_title = getPageTitle('感謝您的報名');
+    test.assertTitle(page_title,'Event register page: page title is OK. (' + page_title + ')');
+    this.capture("picture/event_register_3_5.png");
+    test.assertExists('#help p');
+    test.assertSelectorHasText('#help p','一旦您通過審核，我們將會傳一封電子郵件確認信到您的信箱，您可以點選確認信中的連結網址以完成報名程序。');
+  });
+
+  casper.run(function() {
+    test.done();
+  });
 });
 
