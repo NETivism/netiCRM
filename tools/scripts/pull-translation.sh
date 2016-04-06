b5d8800e02d04262071df35e0610ac2274efcadc
Jimmy Huang <jimmy@netivism.com.tw>
2016-04-06 11:50:07 +0800

    Auto update civicrm.mo after pull.

--- tools/scripts/pull-translation.sh
+++ tools/scripts/pull-translation.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 CALLEDPATH=`dirname $0`
 CIVICRMPATH=`cd $CALLEDPATH/../../ && pwd`
+LANGUAGE='zh_TW'
 
 cd $CIVICRMPATH
 tx pull -f
+if [ -d $CALLEDPATH/../../l10n/$LANGUAGE ]; then
+  if [ ! -d $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES ]; then
+    mkdir $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES
+  fi
+  msgfmt $CALLEDPATH/../../l10n/$LANGUAGE/civicrm.po -o $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES/civicrm.mo
+fi
