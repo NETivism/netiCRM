e32100c675745aa92398f73aef639570789e2818
Jimmy Huang <jimmy@netivism.com.tw>
2016-04-11 14:48:19 +0800

    Correct tools not generate civicrm.mo.

--- tools/scripts/pull-translation.sh
+++ tools/scripts/pull-translation.sh
@@ -5,9 +5,9 @@ LANGUAGE='zh_TW'
 
 cd $CIVICRMPATH
 tx pull -f
-if [ -d $CALLEDPATH/../../l10n/$LANGUAGE ]; then
-  if [ ! -d $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES ]; then
-    mkdir $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES
+if [ -d $CIVICRMPATH/l10n/$LANGUAGE ]; then
+  if [ ! -d $CIVICRMPATH/l10n/$LANGUAGE/LC_MESSAGES ]; then
+    mkdir $CIVICRMPATH/l10n/$LANGUAGE/LC_MESSAGES
   fi
-  msgfmt $CALLEDPATH/../../l10n/$LANGUAGE/civicrm.po -o $CALLEDPATH/../../l10n/$LANGUAGE/LC_MESSAGES/civicrm.mo
+  msgfmt $CIVICRMPATH/l10n/$LANGUAGE/civicrm.po -o $CIVICRMPATH/l10n/$LANGUAGE/LC_MESSAGES/civicrm.mo
 fi
