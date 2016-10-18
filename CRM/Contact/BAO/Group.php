643f6109109d03d22b443b89c5faa9dfa73f6821
Jimmy Huang <jimmy@netivism.com.tw>
2016-10-18 15:44:14 +0800

    refs #18780, civi-sa-2016-12 group parent type check.

--- CRM/Contact/BAO/Group.php
+++ CRM/Contact/BAO/Group.php
@@ -352,6 +352,19 @@ class CRM_Contact_BAO_Group extends CRM_Contact_DAO_Group {
       $params['group_type'] = '';
     }
 
+    // CRM-19068.
+    // Validate parents parameter when creating group.
+    if (isset($params['parents'])) {
+      if (is_array($params['parents'])) {
+        foreach ($params['parents'] as $parent => $dc) {
+          CRM_Utils_Type::validate($parent, 'Integer');
+        }
+      }
+      else {
+        CRM_Utils_Type::validate($params['parents'], 'Integer');
+      }
+    }
+
     $group = new CRM_Contact_BAO_Group();
     $group->copyValues($params);
     $group->save();
