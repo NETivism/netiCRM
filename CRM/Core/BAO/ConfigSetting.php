a9b71dc6315439640e50045d34694b1d2ca5cc9f
Jimmy Huang <jimmy@netivism.com.tw>
2017-04-01 14:36:27 +0800

    refs #19957, Add default lcMessages as english.

--- CRM/Core/BAO/ConfigSetting.php
+++ CRM/Core/BAO/ConfigSetting.php
@@ -183,6 +183,9 @@ class CRM_Core_BAO_ConfigSetting {
           unset($defaults[$skip]);
         }
       }
+      if (empty($defaults['lcMessages'])) {
+        $defaults['lcMessages'] = 'en_US';
+      }
 
       // since language field won't be present before upgrade.
       if (CRM_Utils_Array::value('q', $_GET) == 'civicrm/upgrade') {
