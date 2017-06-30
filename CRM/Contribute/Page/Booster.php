978d68bd9a6c0488600949a5a3b9bccd0e5cede3
root <root@debian9-dev.debian9.dev>
2017-07-01 01:05:50 +0800

    refs #18557 23f. Integrate mdl into civicrm and add styles to contribute booster page.

--- CRM/Contribute/Page/Booster.php
+++ CRM/Contribute/Page/Booster.php
@@ -4,21 +4,29 @@ class CRM_Contribute_Page_Booster extends CRM_Core_Page {
     $mainCategories = array(
       ts('Connect Exists Donors') => array(
         array(
+          'id' => 'first-time-donor',
+          'class' => 'mdl-card-theme',
           'title' => ts('First time donation donors'),
           'description' => ts('Send some message to these people who have first time donation to you. This should be first step to maintain your relationship with these donors.'),
           'link' => CRM_Utils_System::url('civicrm/search/FirstTimeDonor'),
         ),
         array(
+          'id' => 'half-year-donor',
+          'class' => 'mdl-card-theme',
           'title' => ts('Donor who donate in last 6 month'),
           'description' => ts('You can send some result or impact of your project to them. And don\'t forget to exclude them in donation letter.'),
           'link' => CRM_Utils_System::url('civicrm/search/HalfYearDonor', 'force=1'),
         ),
         array(
+          'id' => 'contrib-sybnt',
+          'class' => 'mdl-card-theme',
           'title' => ts('Last year but not this year donors'),
           'description' => ts('These supporter have donation last year, but not in this year. Remember to connect them when you have some donation campaign.'),
           'link' => CRM_Utils_System::url('civicrm/search/ContribSYBNT', 'force=1'),
         ),
         array(
+          'id' => 'failed-no-further-donate',
+          'class' => 'mdl-card-theme',
           'title' => ts('After payment failed but not retry in a week'),
           'description' => ts('They may not have enough motivation to complete donation. Instead of contriubtion campaign, send some impact or result of your effort to them.'),
           'link' => CRM_Utils_System::url('civicrm/search/FailedNoFurtherDonate', 'force=1'),
@@ -26,16 +34,22 @@ class CRM_Contribute_Page_Booster extends CRM_Core_Page {
       ),
       ts('Potential Donors') => array(
         array(
+          'id' => 'single-not-recurring',
+          'class' => 'mdl-card-theme',
           'title' => ts('Single donation over three times'),
           'description' => ts('These supporter appeal they are interested in your orgnization. You should invite them to join your recurring campaign.'),
           'link' => CRM_Utils_System::url('civicrm/search/SingleNotRecurring', 'force=1'),
         ),
         array(
+          'id' => 'recur-search',
+          'class' => 'mdl-card-theme',
           'title' => ts('End of recurring contribution'),
           'description' => ts('These supporter will finished their promised recurring contribution. Time to invite them join your next recurring campaign again.'),
           'link' => CRM_Utils_System::url('civicrm/search/RecurSearch', 'mode=booster&force=1'),
         ),
         array(
+          'id' => 'attendee-not-donor',
+          'class' => 'mdl-card-theme',
           'title' => ts('Attendee but not donor'),
           'description' => ts('They join your event, but not become your donor yet. Time to try to invite them.'),
           'link' => CRM_Utils_System::url('civicrm/search/AttendeeNotDonor', 'force=1'),
--- templates/CRM/Contribute/Page/Booster.tpl
+++ templates/CRM/Contribute/Page/Booster.tpl
@@ -1,11 +1,17 @@
-{foreach from=$main_categories key=main_title item=items}
-<h2>{$main_title}</h2>
-{foreach from=$items key=weight item=card}
-<div class="mdl-card">
-  <a href="{$card.link}">
-  <div class="mdl-card__title">{$card.title}</div>
-  <div class="mdl-card__supporting-text">{$card.description}</div>
-  </a>
+<div class="crm-contribute-booster">
+  {foreach from=$main_categories key=main_title item=items}
+  <div class="crm-section">
+    <h2 class="crm-section-title">{$main_title}</h2>
+    <div class="crm-section-content">
+    {foreach from=$items key=weight item=card}
+      <div id="{$card.id}" class="mdl-card mdl-shadow--2dp {$card.class}">
+        <a href="{$card.link}">
+          <div class="mdl-card__title"><h3 class="mdl-card__title-text">{$card.title}</h3></div>
+          <div class="mdl-card__supporting-text">{$card.description}</div>
+        </a>
+      </div>
+    {/foreach}{* end items*}
+    </div>
+  </div>
+  {/foreach}{*end main_categories*}
 </div>
-{/foreach}{* end items*}
-{/foreach}{*end main_categories*}
