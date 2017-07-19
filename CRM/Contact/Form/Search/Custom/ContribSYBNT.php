f904e08a82f3251f4aef214c28dc169ba5cce834
Jimmy Huang <jimmy@netivism.com.tw>
2017-07-19 19:44:28 +0800

    refs #18562, improve some interface.

--- CRM/Contact/Form/Search/Custom/ContribSYBNT.php
+++ CRM/Contact/Form/Search/Custom/ContribSYBNT.php
@@ -102,6 +102,11 @@ class CRM_Contact_Form_Search_Custom_ContribSYBNT implements CRM_Contact_Form_Se
     CRM_Contribute_Page_Booster::setBreadcrumb();
   }
 
+  function setTitle() {
+    $title = ts('Last year but not this year donors');
+    CRM_Utils_System::setTitle($title);
+  }
+
   function count() {
     $sql = $this->all();
 
