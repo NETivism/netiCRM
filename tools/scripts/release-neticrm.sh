8bc52448f2adba84ed16918694009c7979c515b0
Jimmy Huang <jimmy@netivism.com.tw>
2017-08-16 17:20:22 +0800

    Change release major version.

--- tools/scripts/release-neticrm.sh
+++ tools/scripts/release-neticrm.sh
@@ -2,7 +2,7 @@
 CALLEDPATH=`dirname $0`
 CIVICRMPATH=`cd $CALLEDPATH/../../ && pwd`
 LANGUAGE='zh_TW'
-MAJOR_VERSION='2.6'
+MAJOR_VERSION='2.7'
 
 neticrm_merge(){
   TAG=`git tag | grep "^$MAJOR_VERSION" | awk -F "." '{print $3}' | sort -nr | head -n 1`
