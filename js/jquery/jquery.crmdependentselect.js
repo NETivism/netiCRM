8e98b2c68c6bfdfd0cd61c22e7d55efb774618a6
Jimmy Huang <jimmy@netivism.com.tw>
2018-07-24 17:46:22 +0800

    refs #22685, add ajax form support

--- js/jquery/jquery.crmdependentselect.js
+++ js/jquery/jquery.crmdependentselect.js
@@ -1,20 +1,24 @@
 (function($) {
 $.fn.crmDependentSelect = function() {
-  $(".crm-container select[data-parent]").each(function(){
-    var parentId = $(this).data('parent');
-    var $child = $(this);
-    if (parentId) {
-      var parentId = 'custom_'+parentId;
-      var $parent = $(".crm-container select[name^="+parentId+"]");
-      $child.find('option:not(:checked)').hide();
-      $child.find('option[value=""]').show();
-      $parent.change(function(){
-        var currentSelection = $parent.val();
-        $child.find('option').show();
-        $child.find('option[value=""]').prop("selected", true);
-        $child.find('option[data-parent-filter!="'+currentSelection+'"]').hide();
-      });
-    }
+  $(".crm-container").on("focus", "select:not(.depended)", function(){
+    $(this).addClass("depended");
+    $(".crm-container select[data-parent]").each(function(){
+      $(this).addClass("depended");
+      var parentId = $(this).data('parent');
+      var $child = $(this);
+      if (parentId) {
+        var parentId = 'custom_'+parentId;
+        var $parent = $(".crm-container select[name^="+parentId+"]");
+        $child.find('option:not(:checked)').hide();
+        $child.find('option[value=""]').show();
+        $parent.change(function(){
+          var currentSelection = $parent.val();
+          $child.find('option').show();
+          $child.find('option[value=""]').prop("selected", true);
+          $child.find('option[data-parent-filter!="'+currentSelection+'"]').hide();
+        });
+      }
+    });
   });
 }
 })(jQuery); 
