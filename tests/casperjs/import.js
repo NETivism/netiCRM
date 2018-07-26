33243ad054e4def88884f134efd73a569e3e2e24
Chang Shu-Huai <junsuwhy@gmail.com>
2018-07-26 15:09:59 +0800

    Refs #21806. Fixed import test

--- tests/casperjs/import.js
+++ tests/casperjs/import.js
@@ -2,7 +2,12 @@
 // phantom.injectJs('/usr/local/lib/node_modules/casperjs/bin/bootstrap.js');
 
 var system = require('system');
+var fs = require('fs');
 var port = system.env.RUNPORT;
+
+var pathParts = fs.absolute(casper.test.currentTestFile).split('/');
+pathParts.pop();
+var currentTestFolder = pathParts.join('/') + "/";
 
 var url = (port == '80') ? 'https://127.0.0.1/' : 'http://127.0.0.1:' + port + '/';
 var item = {
@@ -76,7 +81,7 @@ var item = {
 // 0. Login
 casper.test.begin('Page output correct test', 1, function suite(test) {
   casper.start(item.url_prefix, function() {
-    casper.capture("picture/import_task_login_0.png");
+    casper.capture(currentTestFolder+"picture/import_task_login_0.png");
     test.assertExists('#user-login-form', "Found login form");
     this.fill('#user-login-form', {
       'name':'admin',
@@ -84,8 +89,8 @@ casper.test.begin('Page output correct test', 1, function suite(test) {
     }, true);
   });
 
-  casper.waitForUrl(/user/, function(test){
-    casper.capture("picture/import_task_login_1.png");
+  casper.waitForSelector('body.logged-in', function(test){
+    casper.capture(currentTestFolder+"picture/import_task_login_1.png");
   });
 
   casper.run(function() {
@@ -103,28 +108,33 @@ item.task.forEach(function(task, i){
       casper.echo('** Step 1: Enter Upload File Page. **');
       casper.echo('=====================================');
       test.assertExists('#skipColumnHeader', 'Skip Column Header Field is Exist.');
-      casper.capture("picture/import_task_"+i+"_1.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_1.png");
     });
 
     casper.then(function(){
       casper.echo('  - Upload File.');
-      casper.page.uploadFile('#uploadFile', 'files/import.csv');
-      casper.capture("picture/import_task_"+i+"_2.png");
+      casper.page.uploadFile('#uploadFile', currentTestFolder+'files/import.csv');
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_2.png");
     });
 
     casper.then(function(){
       casper.echo('  - Click next button.');
       var form_name = (task.form_name ? task.form_name : 'UploadFile');
       casper.click('input[name="_qf_'+form_name+'_upload"]');
-      casper.capture("picture/import_task_"+i+"_3.png");
-    })
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_3.png");
+    });
+
+    // casper.waitForSelector('.crm-error', function(){
+    //   text = this.evaluate( function(){ return __utils__.findOne('.crm-error li').textContent; } );
+    //   console.log('Messages : '+text);
+    // });
 
     casper.waitForUrl(/_qf_MapField_display/, function(){
       casper.echo('==================================');
       casper.echo('** Step 2: Enter MapField page. **');
       casper.echo('==================================');
       test.assertExists('#_qf_MapField_next-top', 'Success enter MapField Page!!!');
-      casper.capture("picture/import_task_"+i+"_4.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_4.png");
     });
 
     casper.then(function(){
@@ -132,7 +142,7 @@ item.task.forEach(function(task, i){
         casper.echo('  - Select the mapping field.');
         this.fill('form#MapField', task.fields, false);
       }
-      casper.capture("picture/import_task_"+i+"_5.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_5.png");
       casper.echo('  - Click next button.');
       this.click('input[name="_qf_MapField_next"]');
     })
@@ -141,7 +151,7 @@ item.task.forEach(function(task, i){
       casper.echo('=================================');
       casper.echo('** Step 3: Enter Preview page. **');
       casper.echo('=================================');
-      casper.capture("picture/import_task_"+i+"_6.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_6.png");
       test.assertExists('input[name="_qf_Preview_next"]', 'Success enter Preview Page!!!');
     });
 /*
@@ -153,14 +163,14 @@ item.task.forEach(function(task, i){
         return true;
       };
       casper.click('input[name="_qf_Preview_next"]');
-      casper.capture("picture/import_task_"+i+"_5.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_5.png");
     });
 
     casper.waitForUrl(/_qf_Summary_display/, function(){
-      casper.capture("picture/import_task_0_6.png");
+      casper.capture(currentTestFolder+"picture/import_task_0_6.png");
       test.assertExists('#_qf_Summary_next-top', 'Success enter Summary Page!!!');
     }, function timeout(){
-      casper.capture("picture/import_task_"+i+"_6_.png");
+      casper.capture(currentTestFolder+"picture/import_task_"+i+"_6_.png");
       casper.echo('Fail to import.');
     }, 10000);
 */
