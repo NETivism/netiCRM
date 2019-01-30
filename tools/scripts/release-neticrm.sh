25705687e42fbf09893216920686b044c8273def
Jimmy Huang <jimmy@netivism.com.tw>
2019-01-30 13:36:13 +0800

    Change version to 3.1

--- tools/scripts/release-neticrm.sh
+++ tools/scripts/release-neticrm.sh
@@ -2,7 +2,7 @@
 CALLEDPATH=`dirname $0`
 CIVICRMPATH=`cd $CALLEDPATH/../../ && pwd`
 LANGUAGE='zh_TW'
-MAJOR_VERSION='3.0'
+MAJOR_VERSION='3.1'
 
 neticrm_merge(){
   TAG=`git tag | grep "^$MAJOR_VERSION" | awk -F "." '{print $3}' | sort -nr | head -n 1`
