62edb13310144ddb0b7b61cd85b9443e22311bf8
Chang Shu-Huai <junsuwhy@gmail.com>
2019-03-07 16:09:07 +0800

    Refs #24805, Add tappay api and database.

---
+++ CRM/Contribute/DAO/TapPay.php
@@ -0,0 +1,300 @@
+<?php
+/*
++--------------------------------------------------------------------+
+| CiviCRM version 3.3                                                |
++--------------------------------------------------------------------+
+| Copyright CiviCRM LLC (c) 2004-2010                                |
++--------------------------------------------------------------------+
+| This file is a part of CiviCRM.                                    |
+|                                                                    |
+| CiviCRM is free software; you can copy, modify, and distribute it  |
+| under the terms of the GNU Affero General Public License           |
+| Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+|                                                                    |
+| CiviCRM is distributed in the hope that it will be useful, but     |
+| WITHOUT ANY WARRANTY; without even the implied warranty of         |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+| See the GNU Affero General Public License for more details.        |
+|                                                                    |
+| You should have received a copy of the GNU Affero General Public   |
+| License and the CiviCRM Licensing Exception along                  |
+| with this program; if not, contact CiviCRM LLC                     |
+| at info[AT]civicrm[DOT]org. If you have questions about the        |
+| GNU Affero General Public License or the licensing of CiviCRM,     |
+| see the CiviCRM license FAQ at http://civicrm.org/licensing        |
++--------------------------------------------------------------------+
+*/
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2010
+ * $Id$
+ *
+ */
+require_once 'CRM/Core/DAO.php';
+require_once 'CRM/Utils/Type.php';
+class CRM_Contribute_DAO_TapPay extends CRM_Core_DAO
+{
+  /**
+   * static instance to hold the table name
+   *
+   * @var string
+   * @static
+   */
+  static $_tableName = 'civicrm_contribution_tappay';
+  /**
+   * static instance to hold the field values
+   *
+   * @var array
+   * @static
+   */
+  static $_fields = null;
+  /**
+   * static instance to hold the FK relationships
+   *
+   * @var string
+   * @static
+   */
+  static $_links = null;
+  /**
+   * static instance to hold the values that can
+   * be imported / apu
+   *
+   * @var array
+   * @static
+   */
+  static $_import = null;
+  /**
+   * static instance to hold the values that can
+   * be exported / apu
+   *
+   * @var array
+   * @static
+   */
+  static $_export = null;
+  /**
+   * static value to see if we should log any modifications to
+   * this table in the civicrm_log table
+   *
+   * @var boolean
+   * @static
+   */
+  static $_log = false;
+  /**
+   * TapPay ID
+   *
+   * @var int unsigned
+   */
+  public $id;
+  /**
+   * Contribuution ID
+   *
+   * @var int unsigned
+   */
+  public $contribution_id;
+  /**
+   * Contribuution Recur ID
+   *
+   * @var int unsigned
+   */
+  public $contribution_recur_id;
+  /**
+   * Used in parameters of transact. Same as contribution trxn id.
+   *
+   * @var string
+   */
+  public $order_number;
+  /**
+   * Card Secret
+   *
+   * @var string
+   */
+  public $card_secret;
+  /**
+   * Card Key
+   *
+   * @var string
+   */
+  public $card_key;
+  /**
+   * Record Trade ID
+   *
+   * @var string
+   */
+  public $rec_trade_id;
+  /**
+   * The first 6 words of card number.
+   *
+   * @var int unsigned
+   */
+  public $bincode;
+  /**
+   * The last four words of card number.
+   *
+   * @var int unsigned
+   */
+  public $lastfour;
+  /**
+   * class constructor
+   *
+   * @access public
+   * @return civicrm_contribution_tappay
+   */
+  function __construct()
+  {
+    parent::__construct();
+  }
+  /**
+   * return foreign links
+   *
+   * @access public
+   * @return array
+   */
+  function &links()
+  {
+    if (!(self::$_links)) {
+      self::$_links = array(
+        'contribution_id' => 'civicrm_contribution:id',
+        'contribution_recur_id' => 'civicrm_contribution_recur:id',
+      );
+    }
+    return self::$_links;
+  }
+  /**
+   * returns all the column names of this table
+   *
+   * @access public
+   * @return array
+   */
+  function &fields()
+  {
+    if (!(self::$_fields)) {
+      self::$_fields = array(
+        'tappay_id' => array(
+          'name' => 'id',
+          'type' => CRM_Utils_Type::T_INT,
+          'title' => ts('TapPay ID') ,
+          'required' => true,
+        ) ,
+        'contribution_id' => array(
+          'name' => 'contribution_id',
+          'type' => CRM_Utils_Type::T_INT,
+          'title' => ts('Contribuution ID') ,
+          'required' => true,
+          'FKClassName' => 'CRM_Contribute_DAO_Contribution',
+        ) ,
+        'contribution_recur_id' => array(
+          'name' => 'contribution_recur_id',
+          'type' => CRM_Utils_Type::T_INT,
+          'title' => ts('Contribuution Recur ID') ,
+          'FKClassName' => 'CRM_Contribute_DAO_ContributionRecur',
+        ) ,
+        'order_number' => array(
+          'name' => 'order_number',
+          'type' => CRM_Utils_Type::T_STRING,
+          'title' => ts('Contribution Trxn ID') ,
+          'maxlength' => 255,
+          'size' => CRM_Utils_Type::HUGE,
+        ) ,
+        'card_secret' => array(
+          'name' => 'card_secret',
+          'type' => CRM_Utils_Type::T_STRING,
+          'title' => ts('Card Secret') ,
+          'maxlength' => 255,
+          'size' => CRM_Utils_Type::HUGE,
+        ) ,
+        'card_key' => array(
+          'name' => 'card_key',
+          'type' => CRM_Utils_Type::T_STRING,
+          'title' => ts('Card Key') ,
+          'maxlength' => 255,
+          'size' => CRM_Utils_Type::HUGE,
+        ) ,
+        'rec_trade_id' => array(
+          'name' => 'rec_trade_id',
+          'type' => CRM_Utils_Type::T_STRING,
+          'title' => ts('Record Trade ID') ,
+          'maxlength' => 255,
+          'size' => CRM_Utils_Type::HUGE,
+        ) ,
+        'bincode' => array(
+          'name' => 'bincode',
+          'type' => CRM_Utils_Type::T_INT,
+          'title' => ts('Bin Code') ,
+        ) ,
+        'lastfour' => array(
+          'name' => 'lastfour',
+          'type' => CRM_Utils_Type::T_INT,
+          'title' => ts('Last Four') ,
+        ) ,
+      );
+    }
+    return self::$_fields;
+  }
+  /**
+   * returns the names of this table
+   *
+   * @access public
+   * @return string
+   */
+  function getTableName()
+  {
+    return self::$_tableName;
+  }
+  /**
+   * returns if this table needs to be logged
+   *
+   * @access public
+   * @return boolean
+   */
+  function getLog()
+  {
+    return self::$_log;
+  }
+  /**
+   * returns the list of fields that can be imported
+   *
+   * @access public
+   * return array
+   */
+  function &import($prefix = false)
+  {
+    if (!(self::$_import)) {
+      self::$_import = array();
+      $fields = & self::fields();
+      foreach($fields as $name => $field) {
+        if (CRM_Utils_Array::value('import', $field)) {
+          if ($prefix) {
+            self::$_import['contribution_tappay'] = & $fields[$name];
+          } else {
+            self::$_import[$name] = & $fields[$name];
+          }
+        }
+      }
+    }
+    return self::$_import;
+  }
+  /**
+   * returns the list of fields that can be exported
+   *
+   * @access public
+   * return array
+   */
+  function &export($prefix = false)
+  {
+    if (!(self::$_export)) {
+      self::$_export = array();
+      $fields = & self::fields();
+      foreach($fields as $name => $field) {
+        if (CRM_Utils_Array::value('export', $field)) {
+          if ($prefix) {
+            self::$_export['contribution_tappay'] = & $fields[$name];
+          } else {
+            self::$_export[$name] = & $fields[$name];
+          }
+        }
+      }
+    }
+    return self::$_export;
+  }
+}
