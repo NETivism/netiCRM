cdc58630ab78e92900b030abd593eaf25c7bea71
Jimmy Huang <jimmy@netivism.com.tw>
2019-08-05 16:25:11 +0800

    refs #25962, add check-in function and translation

--- CRM/Event/Form/Registration/ParticipantCheckin.php
+++ CRM/Event/Form/Registration/ParticipantCheckin.php
@@ -57,9 +57,30 @@ class CRM_Event_Form_Registration_ParticipantCheckin extends CRM_Event_Form_Regi
       $this->assign("display_name", $displayName); 
       $this->assign("event", $this->_event);
       $participantStatus = CRM_Event_PseudoConstant::participantstatus();
+      $participantStatusPositive = CRM_Event_PseudoConstant::participantStatus( null, "class = 'Positive'" );
       $attendedStatusId = array_search('Attended', $participantStatus);
-      $currentStatus = $participantStatus[$this->_statusId];
-      if ($this->_statusId != $attendedStatusId) {
+      $this->assign('participant_id', $this->_participantId);
+      $this->assign('contact_id', $this->_contactId);
+      if (empty($participantStatusPositive[$this->_statusId])){
+        $this->assign('status', ts($participantStatus[$this->_statusId]));
+        $this->assign("status_message", ts("This participant is not registered."));
+        $this->addRule("qfKey", ts('This participant is not registered.'), 'integer');
+        $this->assign("pending", 1);
+      }
+      elseif ($this->_statusId == $attendedStatusId) {
+        $this->assign('status', ts($participantStatus[$this->_statusId]));
+        $this->assign("status_message", ts("This participant already checked in before. No further action needed."));
+        $this->addRule("qfKey", ts('This participant already checked in before. No further action needed.'), 'integer');
+        // find check-in date in activity
+        $activityTypeId = CRM_Core_OptionGroup::getValue('activity_type', 'Event Registration', 'name');
+        $checkinDate = CRM_Core_DAO::singleValueQuery("SELECT activity_date_time FROM civicrm_activity WHERE activity_type_id = %1 AND subject LIKE '%Attended%' AND source_record_id = %2 ORDER BY activity_date_time DESC LIMIT 1", array(
+          1 => array($activityTypeId, 'Integer'),
+          2 => array($this->_participantId, 'Integer'),
+        ));
+        $this->assign("checkin_date", $checkinDate);
+        $this->assign("attended", 1);
+      }
+      else {
         // show status change to manager
         $this->assign('status_before', ts($participantStatus[$this->_statusId]));
         $this->assign('status_after', ts($participantStatus[$attendedStatusId]));
@@ -69,17 +90,27 @@ class CRM_Event_Form_Registration_ParticipantCheckin extends CRM_Event_Form_Regi
         ));
         $this->addButtons($buttons);
         $this->assign("status_message", ts("Press confirm to process check-in for this participant."));
-        $this->assign("current_date", CRM_Utils_Date::customFormat(date('YmdHis')));
-        $this->assign("checked", 0);
-      }
-      else {
-        $this->assign("status_message", ts("This participant already checked in before. No further action needed."));
-        $this->assign("checked", 1);
+        $this->assign("current_date", date('Y-m-d H:i:s'));
+        $session = CRM_Core_Session::singleton();
+        $session->pushUserContext(CRM_Utils_System::url(CRM_Utils_System::currentPath(),
+          "reset=1&participantId={$this->_participantId}&cs={$this->_csString}"
+        ));
       }
     }
   }
 
   public function postProcess() {
-
+    $participantStatus = CRM_Event_PseudoConstant::participantstatus();
+    $attendedStatusId = array_search('Attended', $participantStatus);
+    $params = array(
+      'id' => $this->_participantId,
+      'status_id' => $attendedStatusId,
+    );
+    CRM_Event_BAO_Participant::create($params);
+    $displayName = CRM_Core_DAO::getFieldValue("CRM_Contact_DAO_Contact", $this->_contactId, 'display_name');
+    CRM_Core_Session::setStatus(ts("%1 status has been updated to %2.", array(
+      1 => $displayName,
+      2 => ts("Attended"),
+    )));
   }
 }
