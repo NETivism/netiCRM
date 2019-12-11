33af514504d5c640182a99a53c3807133d878486
Jimmy Huang <jimmy@netivism.com.tw>
2019-12-11 13:25:48 +0800

    refs #26013, fixes custom field search not appear when element per line not set

--- CRM/Core/BAO/CustomField.php
+++ CRM/Core/BAO/CustomField.php
@@ -835,13 +835,13 @@ class CRM_Core_BAO_CustomField extends CRM_Core_DAO_CustomField {
         );
         $check = array();
         foreach ($customOption as $v => $l) {
-          $check[] = &$qf->addElement('advcheckbox', $v, NULL, $l);
+          $check[] = &$qf->addElement('checkbox', $v, NULL, $l);
         }
         if ($search && count($check) > 1) {
-          $check[] = &$qf->addElement('advcheckbox', 'CiviCRM_OP_OR', NULL, ts('Check to match ANY; uncheck to match ALL'));
+          $check[] = &$qf->addElement('checkbox', 'CiviCRM_OP_OR', NULL, ts('Check to match ANY; uncheck to match ALL'));
         }
         if ($search) {
-          $check[] = &$qf->addElement('advcheckbox', 'CiviCRM_OP_EXCLUDE', NULL, ts('Check to Exclude; uncheck to Include'));
+          $check[] = &$qf->addElement('checkbox', 'CiviCRM_OP_EXCLUDE', NULL, ts('Check to Exclude; uncheck to Include'));
         }
         $qf->addGroup($check, $elementName, $label);
         if (($useRequired || ($useRequired && $field->is_required)) && !$search) {
