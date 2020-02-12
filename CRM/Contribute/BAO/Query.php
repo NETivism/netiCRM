cb3d7cf41f5b92ba61e0490d9343168e33398c40
Jimmy Huang <jimmy@netivism.com.tw>
2020-02-12 11:02:47 +0800

    refs #27215, change input type of id search

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -787,8 +787,8 @@ class CRM_Contribute_BAO_Query {
     require_once 'CRM/Utils/Money.php';
 
     //added contribution source
-    $form->addElement('text', 'contribution_id', ts('Contribution ID'));
-    $form->addElement('text', 'contribution_recur_id', ts('Recurring Contributions ID'));
+    $form->addNumber('contribution_id', ts('Contribution ID'));
+    $form->addNumber('contribution_recur_id', ts('Recurring Contributions ID'));
     $form->addElement('text', 'contribution_source', ts('Contribution Source'), CRM_Core_DAO::getAttribute('CRM_Contribute_DAO_Contribution', 'source'));
 
     $form->addDate('contribution_created_date_low', ts('Created Date - From'), FALSE, array('formatType' => 'searchDate'));
