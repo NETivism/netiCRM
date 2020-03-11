89b2c868254fe8b62d4c984dcc516d52fa247633
poliphilo <poliphilo@netivism.com.tw>
2020-03-11 12:55:24 +0800

    refs #23719. Add nmeEditor main function (nmeElem, nmeBlock, nmeBlockControl, etc..) and set plugin default options.

--- packages/mailingEditor/mailingEditor.js
+++ packages/mailingEditor/mailingEditor.js
@@ -36,12 +36,31 @@
     },
     _debugMode = false,
 		_data = {},
-		_dataLoadMode = "api",
+		_dataLoadMode = "field",
     _nme, // plugin object
 		_nmeOptions = {},
 		_nmeAPI = window.location.origin + "/api/",
 		_container,
-		_main = "." + NME_MAIN;
+		_main = "." + NME_MAIN,
+		_controlIconClass = {
+			drag: "zmdi-arrows",
+			prev: "zmdi-long-arrow-up",
+			next: "zmdi-long-arrow-down",
+			clone: "zmdi-collection-plus",
+			delete: "zmdi-delete",
+			link: "zmdi-link",
+			image: "zmdi-image",
+			style: "zmdi-format-color-fill"
+		},
+		_editActions = {
+			default: ["clone", "delete"],
+			extended: {
+				image: ["link", "image"],
+				title: ["link", "style"],
+				paragraph: ["style"],
+				button: ["link", "style"]
+			}
+		};
 
 	/**
 	 * ============================
@@ -58,7 +77,17 @@
 
   var _isNumeric = function(n) { 
     return !isNaN(parseFloat(n)) && isFinite(n);
-  }
+	}
+
+	var _isJsonString = function(str) {
+		try {
+			var json = JSON.parse(str);
+			return (typeof json === "object");
+		} catch (e) {
+			_debug("===== Source data is not json. =====");
+			return false;
+		}
+	}
 
 	var _objIsEmpty = function(obj) {
     for (var key in obj) {
@@ -112,11 +141,141 @@
         location.hash = hash;
       }
     }
+	}
+
+  var _renderID = function(str, len) {
+    var str = typeof str !== "undefined" ? str : "";
+    var len = typeof len !== "undefined" ? len : 10;
+    var allow = "abcdefghijklmnopqrstuvwxyz0123456789";
+    // var allow = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var output = "";
+
+    if (str) {
+      output = str + "-";
+    }
+
+    for (var i = 0; i < len; i++) {
+      output += allow.charAt(Math.floor(Math.random() * allow.length));
+    }
+
+    if (output) {
+      return output;
+		}
   }
 
 	/**
 	 * Main
 	 */
+	var _nmeData = {
+		get: {
+			local: function(data) {
+				if (typeof data !== "undefined") {
+					return data;
+				}
+			},
+			field: function(selector) {
+				let $field = $(selector);
+
+				if ($field.length) {
+					let dataString = $field.val();
+
+					if (_isJsonString(dataString)) {
+						_data = JSON.parse(dataString);
+						_debug(_data, "Source data");
+					}
+				}
+			}
+		},
+		update: function() {
+
+		}
+	};
+
+	var _nmeElem = {
+		render: function(type, mode, source) {
+			let output = "",
+					elemType = typeof type !== undefined ? type : "",
+					elemMode = typeof mode !== undefined ? mode : "view",
+					targetID = typeof source !== undefined ? source : null;
+
+			switch (elemType) {
+				case "image":
+					if (targetID) {
+						output =  "<img src='" + _data.blocks[targetID].data + "' alt=''>";
+					}
+					else {
+						output = "<img src='https://unsplash.it/1360/600?image=972' alt=''>";
+					}
+					break;
+
+				case "title":
+					if (elemMode == "edit") {
+						if (targetID) {
+							output =  "";
+						}
+						else {
+							output = "";
+						}
+					}
+					else {
+						if (targetID) {
+							output =  "";
+						}
+						else {
+							output = "";
+						}
+					}
+					break;
+
+				case "paragraph":
+					break;
+
+				case "button":
+					break;
+			}
+
+			return output;
+		}
+	};
+
+	var _nmeBlock = {
+		add: function(type, mode, id) {
+			let output = "",
+					blockID = _data.blocks[id] ? id : type + "-" + _renderID(),
+					blockMode = typeof mode !== undefined ? mode : "view",
+					blockType = typeof type !== undefined ? type : "",
+					blockDomID = "nme-block-" + elemID,
+					blockDomClass = "nme-block",
+					source = _data.blocks[id] ? blockID : null;
+
+			// nmeBlock: start
+			output += "<div id='" + blockDomID + "' data-id='" + blockDomID + "' class='" + blockDomClass + "' data-type='" + blockType + "'>" +
+				"<div class='nme-block-inner'>" +
+				"<div class='nme-block-content'>";
+
+			// nmeBlock item
+			output += "<div class='nme-" + blockType + " nme-item'>" + _nmeElem.render(blockType, blockMode, source) + "</div>";
+
+			// nmeBlock content: end
+			output += "</div>";
+
+			// If the mode is 'edit', render nmeBlock control buttons.
+			if (blockMode == "edit") {
+				_nmeBlockControl.render(blockType);
+			}
+
+			output += "</div>"; // nmeBlock inner: end
+			output += "</div>"; // nmeBlock: end
+
+		},
+		update: function() {
+
+		},
+		delete: function() {
+
+		}
+	};
+
 	var _nmeMain = {
 		render: function() {
 			if ($(_main).length == 0) {
@@ -126,22 +285,11 @@
 	};
 
 	/* Main Help function */
-	var _checkSnaInstance = function() {
+	var _checkNmeInstance = function() {
 		if(!$.nmEditor.instance) {
 			_nme = new nmEditor();
 			_nme.init();
 			$.nmEditor.instance = _nme;
-		}
-	};
-
-	var _getData = {
-		local: function(data) {
-			if (typeof data !== "undefined") {
-				return data;
-			}
-		},
-		api: function(url) {
-			return _getJSON(url);
 		}
 	};
 
@@ -162,79 +310,117 @@
 		});
 	};
 
-	var _nmeBlockControl = function() {
-		$(".nme-block-control").on("click", ".handle-btn", function() {
-			let $handle = $(this),
-					handleType = $handle.data("type"),
-					$block = $handle.closest(".nme-block"),
-					blockID = $block.attr("id"),
-					blockType = $block.data("type"),
-					$target = $block.find(".nme-" + blockType);
+	var _nmeBlockControl = {
+		render: function(type) {
+			let output = "",
+					blockType = typeof type !== undefined ? type : "";
 
-			// $block.addClass(ACTIVE_CLASS);
-			// move
-			
-			// actions
-			// image
-			if (handleType == "image") {
-				nmeImceTargetID = blockID;
-				var win = window.open('/imce&app=nme|sendto@nmeImce', 'nme_imce', 'width=640, height=480');
-			}
+			// nmeBlock control: start
+			output += "<div class='nme-block-control'>";
 
-			// link
-			if (handleType == "link") {
-				let	editBlockID = "nme-edit-" + blockID,
-						editItemID = "nme-edit-" + handleType + "-item-" + blockID;
-						
-				if ($target.length && !$target.find(".nme-edit").length) {
-					let editItemVal = $target.data("link") ? $target.data("link") : "";
-					let nmeEdit = "<div id='" + editBlockID + "' class='nme-edit' data-target='" + blockID + "'>" +
-						"<div class='" + INNER_CLASS + "'>" +
-						"<div class='nme-edit-link-item nme-edit-item'>" +
-						"<div class='nme-edit-item-label'>" +
-						"<label for='" + editItemID + "'>請輸入網址</label>" +
-						"</div>" + // .nme-edit-item-label
-						"<div class='nme-edit-item-content'>" +
-						"<input id='" + editItemID + "' name='" + editItemID + "' type='text' placeholder='http://' value='" + editItemVal + "'>" +
-						"</div>" + // .nme-edit-item-content
-						"</div>" + // .nme-edit-item
-						"<div class='nme-edit-actions'>" +
-						"<a href='#' class='nme-edit-submit nme-edit-action btn' data-type='submit'>儲存</a>" +
-						"<a href='#' class='nme-edit-cancel nme-edit-action btn' data-type='cancel'>取消</a>" +
-						"</div>" + // .nme-edit-actions
-						"</div>" + // .inner
-						"</div>"; // .nme-edit
+			// nmeBlock control - move
+			output + "<div class='nme-block-move'>" +
+			"<span class='handle-drag handle-btn' data-type='drag'><i class='zmdi " + _controlIconClass.drag + "'></i></span>" +
+			"<span class='handle-prev handle-btn' data-type='prev'><i class='zmdi " + _controlIconClass.prev + "'></i></span>" +
+			"<span class='handle-next handle-btn' data-type='next'><i class='zmdi " + _controlIconClass.next + "'></i></span>" +
+			"</div>";
 
+			// nmeBlock control - actions
+			output += "<div class='nme-block-actions'>";
 
-					$block.addClass(EDIT_CLASS);
+			// nmeBlock control - extended actions
+			if (_editActions.extended[blockType]) {
+				let elemExtendedActions = _editActions.extended[blockType];
 
-					// 將編輯表單放入對應的元件內
-					$target.addClass(EDIT_CLASS).append(nmeEdit);
-					let $editBlock = $("#" + editBlockID),
-							$editItem = $("#" + editItemID);
-
-					// 編輯表單的按鈕事件：送出與取消
-					$editBlock.on("click", ".nme-edit-action", function(event) {
-						event.preventDefault();
-
-						let $action = $(this),
-								actionType = $action.data("type");
-
-						if (actionType == "submit") {
-							let editItemVal = $editItem.val();
-
-							if ($.trim(editItemVal)) {
-								$target.attr("data-link", editItemVal);
-							}
-						}
-
-						$editBlock.remove();
-						$block.removeClass(EDIT_CLASS);
-						$target.removeClass(EDIT_CLASS);
- 					});
+				for (let k in elemExtendedActions) {
+					let action = elemExtendedActions[k];
+					output += "<span class='handle-" + action + " handle-btn' data-type='" + action + "'><i class='zmdi " + _controlIconClass[action] + "'></i></span>";
 				}
 			}
-		});
+
+			// nmeBlock control - default actions
+			output += "<span class='handle-clone handle-btn' data-type='clone'><i class='zmdi " + _controlIconClass.clone + "'></i></span>" +
+			"<span class='handle-delete handle-btn' data-type='delete'><i class='zmdi " + _controlIconClass.delete + "'></i></span>";
+
+			// nmeBlock control: end
+			output += "</div>";
+
+			return output;
+		},
+		init: function() {
+			$(".nme-block-control").on("click", ".handle-btn", function() {
+				let $handle = $(this),
+						handleType = $handle.data("type"),
+						$block = $handle.closest(".nme-block"),
+						blockID = $block.attr("id"),
+						blockType = $block.data("type"),
+						$target = $block.find(".nme-" + blockType);
+
+				// $block.addClass(ACTIVE_CLASS);
+				// move
+
+				// actions
+				// image
+				if (handleType == "image") {
+					nmeImceTargetID = blockID;
+					var win = window.open('/imce&app=nme|sendto@nmeImce', 'nme_imce', 'width=640, height=480');
+				}
+
+				// link
+				if (handleType == "link") {
+					let	editBlockID = "nme-edit-" + blockID,
+							editItemID = "nme-edit-" + handleType + "-item-" + blockID;
+
+					if ($target.length && !$target.find(".nme-edit").length) {
+						let editItemVal = $target.data("link") ? $target.data("link") : "";
+						let nmeEdit = "<div id='" + editBlockID + "' class='nme-edit' data-target='" + blockID + "'>" +
+							"<div class='" + INNER_CLASS + "'>" +
+							"<div class='nme-edit-link-item nme-edit-item'>" +
+							"<div class='nme-edit-item-label'>" +
+							"<label for='" + editItemID + "'>請輸入網址</label>" +
+							"</div>" + // .nme-edit-item-label
+							"<div class='nme-edit-item-content'>" +
+							"<input id='" + editItemID + "' name='" + editItemID + "' type='text' placeholder='http://' value='" + editItemVal + "'>" +
+							"</div>" + // .nme-edit-item-content
+							"</div>" + // .nme-edit-item
+							"<div class='nme-edit-actions'>" +
+							"<a href='#' class='nme-edit-submit nme-edit-action btn' data-type='submit'>儲存</a>" +
+							"<a href='#' class='nme-edit-cancel nme-edit-action btn' data-type='cancel'>取消</a>" +
+							"</div>" + // .nme-edit-actions
+							"</div>" + // .inner
+							"</div>"; // .nme-edit
+
+
+						$block.addClass(EDIT_CLASS);
+
+						// 將編輯表單放入對應的元件內
+						$target.addClass(EDIT_CLASS).append(nmeEdit);
+						let $editBlock = $("#" + editBlockID),
+								$editItem = $("#" + editItemID);
+
+						// 編輯表單的按鈕事件：送出與取消
+						$editBlock.on("click", ".nme-edit-action", function(event) {
+							event.preventDefault();
+
+							let $action = $(this),
+									actionType = $action.data("type");
+
+							if (actionType == "submit") {
+								let editItemVal = $editItem.val();
+
+								if ($.trim(editItemVal)) {
+									$target.attr("data-link", editItemVal);
+								}
+							}
+
+							$editBlock.remove();
+							$block.removeClass(EDIT_CLASS);
+							$target.removeClass(EDIT_CLASS);
+						 });
+					}
+				}
+			});
+		}
 	};
 
 	var _nmePanels = function() {
@@ -252,7 +438,7 @@
 	};
 
 	var _nmEditorInit = function() {
-    _debug("===== nmEditor Init =====");
+		_debug("===== nmEditor Init =====");
 
 		if (!$(_container).hasClass(NME_CONTAINER)) {
 			$(_container).addClass(NME_CONTAINER);
@@ -318,7 +504,7 @@
 
       if (allow) {
         if (label) {
-          window.console.log("===== " + label + "=====");
+          window.console.log("===== " + label + " =====");
         }
         
         if (typeof item === "object") {
@@ -372,7 +558,10 @@
 		constructor: nmEditor,
 		init: function() {
       _nmEditorInit();
-      
+
+			if (_dataLoadMode == "field") {
+				_nmeData.get.field(_nmeOptions.dataLoadSource);
+			}
       /*
       if (_dataLoadMode == "api") {
         var dataURL =  _nmeAPI + "/xxx";			    
@@ -395,7 +584,7 @@
       */
 			/*
 			if (_dataLoadMode == "local") {
-				_data = _getData.local();
+				_data = _nmeData.get.local();
 				_nmEditorInit();
 			}
 			*/
@@ -404,7 +593,7 @@
 			_nmeMain.render();
 			_editable();
 			_sortable();
-			_nmeBlockControl();
+			_nmeBlockControl.init();
 			_nmePanels();
 		},
 		open: function(elem) {
@@ -443,27 +632,25 @@
 	// Plugin definition
 	$.fn.nmEditor = function(options) {
     // Extend our default options with those provided
-    _nmeOptions = $.extend({}, $.fn.nmEditor.defaults, options);
+		_nmeOptions = $.extend({}, $.fn.nmEditor.defaults, options);
 
     // Plugin implementation
     _qs = _parseQueryString(_query);
-
-		if (_qs.dataLoadMode) {
-      _dataLoadMode = _qs.dataLoadMode;
-    }
     
-    if (_qs.debug) {
+    if (_nmeOptions.debugMode && _qs.debug) {
       _debugMode = _qs.debug;
     }
 
     _container = this.selector;
-    _checkSnaInstance();
+    _checkNmeInstance();
 
 		return this;
 	};
 	 
 	// Plugin defaults options
 	$.fn.nmEditor.defaults = {
-		// zoom: zoomProps.default
+		dataLoadMode: "field",
+		dataLoadSource: "#mailing_content_data",
+		debugMode: false
 	};
 }(jQuery));
