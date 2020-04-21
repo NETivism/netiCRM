4d00b77e7b0f5c98cdf2b6c199910ba5f3989a19
Jimmy Huang <jimmy@netivism.com.tw>
2020-04-21 12:27:07 +0800

    refs #28162, redirect needs referrer check

--- CRM/Core/Page/SafariFix.php
+++ CRM/Core/Page/SafariFix.php
@@ -3,12 +3,15 @@ class CRM_Core_Page_SafariFix extends CRM_Core_Page {
   function run() {
     $redirect = CRM_Utils_Request::retrieve('ref', 'String', $this);
     setcookie('safariCookieFix', 1, CRM_REQUEST_TIME+86400*7, "/");
-    $url = parse_url($redirect);
-    if ($redirect) {
-      CRM_Utils_System::redirect($redirect);
+    if ($_SERVER['HTTP_REFERER']) {
+      $referrer = parse_url($_SERVER['HTTP_REFERRER']);
+      if ($referrer['host'] == $_SERVER['HTTP_HOST']) {
+        $url = parse_url($redirect);
+        if ($redirect) {
+          CRM_Utils_System::redirect($redirect);
+        }
+      }
     }
-    else {
-      CRM_Utils_System::civiExit();
-    }
+    CRM_Utils_System::civiExit();
   }
 }
