3d1e9bf4e3a5b6ce3ac32764dfa8a7207f29ae44
poliphilonet <poliphilo@netivism.com.tw>
2020-12-01 21:37:17 +0800

    refs #29481. Added inline style whitelist to Quill, allow text-decoration.

--- packages/x-editable/dist/inputs-ext/quill/quill.js
+++ packages/x-editable/dist/inputs-ext/quill/quill.js
@@ -85,6 +85,25 @@ $(function(){
             var quillAlign = Quill.import('attributors/style/align');
             Quill.register(quillAlign, true);
 
+            // Added inline style whitelist to Quill
+            var quillParchment = Quill.import('parchment');
+            var quillParchmentAttrs = {};
+
+            // Inline Style Whitelist: text-decoration
+            quillParchmentAttrs.textDecoration = {};
+            quillParchmentAttrs.textDecoration.attrConfig = {
+                scope: quillParchment.Scope.INLINE,
+                whitelist: ['none', 'underline', 'overline', 'line-through']
+            };
+            quillParchmentAttrs.textDecoration.attrStyle = new quillParchment.Attributor.Style('text-decoration', 'text-decoration', quillParchmentAttrs.textDecoration.attrConfig);
+
+            // Register attributor
+            for (var attr in quillParchmentAttrs) {
+                if (quillParchmentAttrs[attr].attrStyle) {
+                    quillParchment.register(quillParchmentAttrs[attr].attrStyle);
+                }
+            }
+
             var toolbarOptions = [
               ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
               [{ 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466'] }, { 'background': [] }],          // dropdown with defaults from theme
