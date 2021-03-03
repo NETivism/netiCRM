8300eb66b194a61ac2c5e2514a61b21d05016d00
Jimmy Huang <jimmy@netivism.com.tw>
2021-03-03 14:57:04 +0800

    refs #14371, force url to be https start

--- CRM/Mailing/BAO/Mailing.php
+++ CRM/Mailing/BAO/Mailing.php
@@ -1204,8 +1204,9 @@ AND civicrm_contact.is_opt_out =0";
       ),
     );
 
+    $unsubscribeUrl = str_replace(array('&amp;', 'http://'), array('&', 'https://'), $urls['unsubscribeUrl']);
     $headers = array(
-      'List-Unsubscribe' => '<'.str_replace('&amp;', '&', $urls['unsubscribeUrl']).'>'.' ,'."<mailto:{$verp['unsubscribe']}>",
+      'List-Unsubscribe' => '<'.$unsubscribeUrl.'>'.' ,'."<mailto:{$verp['unsubscribe']}>",
       'From' => "\"{$this->from_name}\" <{$this->from_email}>",
       'Sender' => $verp['reply'],
       'Return-Path' => $verp['bounce'],
