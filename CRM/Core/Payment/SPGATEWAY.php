f0c95ca75d9dce57ec9d70b11acf6061f37d3332
Chang Shu-Huai <junsuwhy@gmail.com>
2021-06-01 17:56:15 +0800

    Refs #29617, Modify spgateway submit validate for installments.

--- CRM/Core/Payment/SPGATEWAY.php
+++ CRM/Core/Payment/SPGATEWAY.php
@@ -76,22 +76,25 @@ class CRM_Core_Payment_SPGATEWAY extends CRM_Core_Payment {
     if (!empty($form->get('id'))) {
       $installment = CRM_Core_DAO::getFieldValue('CRM_Contribute_DAO_ContributionRecur', $form->get('id'), 'installments');
       if (!empty($installment)) {
-        $form->registerRule('installments', 'callback', 'validateInstallments', 'CRM_Core_Payment_SPGATEWAY');
-        $form->addRule('id', ts('Installments should be greater than zero.'), 'installments', $form);
         $form->set('original_installments', $installment);
+        $form->addFormRule(array('CRM_Core_Payment_SPGATEWAY', 'validateInstallments'), $form);
       }
     }
   }
 
-  static function validateInstallments($value, $form) {
+  static function validateInstallments($fields, $ignore, $form) {
+    $errors = array();
     $pass = TRUE;
-    $contribution_status_id = $form->_submitValues['contribution_status_id'];
-    $installments = $form->_submitValues['installments'];
+    $contribution_status_id = $fields['contribution_status_id'];
+    $installments = $fields['installments'];
     $original_installments = $form->get('original_installments');
     if ($contribution_status_id == 5 && !empty($original_installments) && $installments <= 0) {
       $pass = FALSE;
     }
-    return $pass;
+    if (!$pass) {
+      $errors['installments'] = ts('Installments should be greater than zero.');
+    }
+    return $errors;
   }
 
   /**
