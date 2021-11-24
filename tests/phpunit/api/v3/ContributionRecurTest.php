bce7a9bb091ab65449d3023dc2379a3e75942266
Jimmy Huang <jimmy@netivism.com.tw>
2021-11-24 11:55:25 +0800

    refs #33108, first runnable contribution recurring api unit test

--- tests/phpunit/api/v3/ContributionRecurTest.php
+++ tests/phpunit/api/v3/ContributionRecurTest.php
@@ -1,5 +1,4 @@
 <?php
-// $Id$
 
 require_once 'CiviTest/CiviUnitTestCase.php';
 class api_v3_ContributionRecurTest extends CiviUnitTestCase {
@@ -8,67 +7,70 @@ class api_v3_ContributionRecurTest extends CiviUnitTestCase {
   protected $ids = array();
   protected $_entity = 'contribution_recur';
   public $_eNoticeCompliant = TRUE;
-  public $DBResetRequired = FALSE; function setUp() {
+
+  function setUp() {
     parent::setUp();
     $this->ids['contact'][0] = $this->individualCreate();
     $this->params = array(
       'version' => 3,
       'contact_id' => $this->ids['contact'][0],
-      'installments' => '12',
+      'amount' => '500.00',
+      'currency' => 'TWD',
+      'frequency_unit' => 'month',
       'frequency_interval' => '1',
-      'amount' => '500',
-      'contribution_status_id' => 1,
-      'start_date' => '2012-01-01 00:00:00',
-      'currency' => 'USD',
-      'frequency_unit' => 'day',
+      'installments' => '12',
+      'start_date' => date('Y-m-25 H:i:s', strtotime('last month')),
+      'create_date' => date('Y-m-01 H:i:s', strtotime('last month')),
+      'cancel_date' => '',
+      'end_date' => '',
+      'processor_id' => '',
+      'external_id' => '',
+      'trxn_id' => CRM_Utils_String::createRandom(10),
+      'invoice_id' => CRM_Utils_String::createRandom(32),
+      'contribution_status_id' => 5, // processing
+      'is_test' => 0,
+      'cycle_day' => 5,
+      'next_sched_contribution' => date('Y-m-25 H:i:s'),
+      'failure_count' => 0,
+      'failure_retry_date' => '',
+      'auto_renew' => 0,
+      'last_execute_date' => '',
     );
   }
 
   function tearDown() {
-    foreach ($this->ids as $entity => $entities) {
-      foreach ($entities as $id) {
-        civicrm_api($entity, 'delete', array('version' => $this->_apiversion, 'id' => $id));
-      }
-    }
-    $tablesToTruncate = array(
-      'civicrm_contribution_type',
-      'civicrm_contribution',
-      'civicrm_contribution_recur',
-      'civicrm_membership',
-    );
-    $this->quickCleanup($tablesToTruncate);
-  }
-
-  public function testCreateContributionRecur() {
-    $result = civicrm_api($this->_entity, 'create', $this->params);
-    $this->documentMe($this->params, $result, __FUNCTION__, __FILE__);
-    $this->assertAPISuccess($result, 'In line ' . __LINE__);
-    $this->assertEquals(1, $result['count'], 'In line ' . __LINE__);
-    $this->assertNotNull($result['values'][$result['id']]['id'], 'In line ' . __LINE__);
-    $this->getAndCheck($this->params, $result['id'], $this->_entity);
   }
 
   public function testGetContributionRecur() {
     $result = civicrm_api($this->_entity, 'create', $this->params);
     $getParams = array(
       'version' => $this->_apiversion,
-      'amount' => '500',
+      'id' => $result['id'],
     );
     $result = civicrm_api($this->_entity, 'get', $getParams);
-    $this->documentMe($getParams, $result, __FUNCTION__, __FILE__);
+    $this->assertAPISuccess($result, 'In line ' . __LINE__);
+  }
+
+  public function testCreateContributionRecur() {
+    $result = civicrm_api($this->_entity, 'create', $this->params);
     $this->assertAPISuccess($result, 'In line ' . __LINE__);
     $this->assertEquals(1, $result['count'], 'In line ' . __LINE__);
+    $this->assertNotNull($result['values'][$result['id']]['id'], 'In line ' . __LINE__);
+    $this->getAndCheck($this->params, $result['id'], $this->_entity);
   }
 
   public function testDeleteContributionRecur() {
     $result = civicrm_api($this->_entity, 'create', $this->params);
-    $deleteParams = array('version' => 3, 'id' => $result['id']);
-    $result = civicrm_api($this->_entity, 'delete', $deleteParams);
-    $this->documentMe($deleteParams, $result, __FUNCTION__, __FILE__);
-    $this->assertAPISuccess($result, 'In line ' . __LINE__);
+    $deleteParams = array(
+      'version' => 3,
+      'id' => $result['id'],
+    );
+    $deleted = civicrm_api($this->_entity, 'delete', $deleteParams);
+    $this->assertAPISuccess($deleted, 'In line ' . __LINE__);
     $checkDeleted = civicrm_api($this->_entity, 'get', array(
       'version' => 3,
-      ));
+      'id' => $deleteParams['id'],
+    ));
     $this->assertEquals(0, $checkDeleted['count'], 'In line ' . __LINE__);
   }
 
