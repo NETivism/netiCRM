06d5a810fbd0893d5dcfb9d327a0bca677ed9848
ChiaChin <chiachin@netivism.com.tw>
2022-05-04 16:36:27 +0800

    refs #32216, gh-46, Change display way to show empty email.

--- CRM/Contribute/Form/Task/PDF.php
+++ CRM/Contribute/Form/Task/PDF.php
@@ -137,6 +137,7 @@ class CRM_Contribute_Form_Task_PDF extends CRM_Contribute_Form_Task {
     $emptyEmailList = implode(",", $emptyEmail);
     $actionName = $this->controller->getActionName($this->_name);
     if ($emailIsEmpty && $actionName[1] == 'display') {
+      $this->assign('emptyEmailList', $emptyEmailList);
       CRM_Core_Session::setStatus(ts("%1 doesn't have email. Skipped receipt generation.", array(1 => $emptyEmailList)));
     }
   }
