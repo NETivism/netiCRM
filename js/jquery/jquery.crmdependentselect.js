821aba16437de9fa346f4446b4d4bbbaa7796c4c
poliphilonet <poliphilo@netivism.com.tw>
2022-06-02 14:50:50 +0800

    refs #32871, gh-43, fixed crmDependentSelect bug when the depended element is preset value.

--- js/jquery/jquery.crmdependentselect.js
+++ js/jquery/jquery.crmdependentselect.js
@@ -1,24 +1,30 @@
 (function($) {
 $.fn.crmDependentSelect = function() {
-  $(".crm-container").on("focus", "select:not(.depended)", function(){
+  $(".crm-container").on("focus", "select:not(.depended)", function() {
     $(this).addClass("depended");
-    $(".crm-container select[data-parent]").each(function(){
+
+    $(".crm-container select[data-parent]").each(function() {
       $(this).addClass("depended");
       var $child = $(this),
-          parentId = $(this).attr('data-parent'),
-          isParentCustom = $(this).attr('data-parent-custom');
+          parentId = $(this).attr("data-parent"),
+          isParentCustom = $(this).attr("data-parent-custom");
 
       if (parentId) {
-        parentId = isParentCustom != 0 ? 'custom_' + parentId : parentId;
-        var $parent = $(".crm-container select[name^="+parentId+"]");
-        $child.find('option:not(:checked)').hide();
-        $child.find('option[value=""]').show();
-        $parent.change(function(){
-          var currentSelection = $parent.val();
-          $child.find('option').show();
-          $child.find('option[value=""]').prop("selected", true);
-          $child.find('option[data-parent-filter!="'+currentSelection+'"]').hide();
-        });
+        parentId = isParentCustom != 0 ? "custom_" + parentId : parentId;
+        var $parent = $(".crm-container select[name^=" + parentId + "]"),
+            parentVal = $parent.val();
+
+        $child.find("option:not([data-parent-filter='" + parentVal + "']").hide();
+        $child.find("option[value='']").show();
+
+        if ($parent.length) {
+          $parent.change(function() {
+            parentVal = $(this).val();
+            $child.find("option").show();
+            $child.find("option[value='']").prop("selected", true);
+            $child.find("option:not([data-parent-filter='" + parentVal + "']").hide();
+          });
+        }
       }
     });
   });
