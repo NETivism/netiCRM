5718a2a45b213cebdb99e14c54530c250d711fba
Chang Shu-Huai <junsuwhy@netivism.com.tw>
2022-07-13 15:40:32 +0800

    Refs #35383, Check recur_id before in tappay contribution info page.

--- CRM/Core/Payment/TapPay.php
+++ CRM/Core/Payment/TapPay.php
@@ -1278,8 +1278,13 @@ LIMIT 0, 100
     $smarty = CRM_Core_Smarty::singleton();
     civicrm_smarty_register_string_resource();
     $updateCardmetaButton = $smarty->fetch('string: {$form.$update_notify.html}');
-    $params = array( 1 => array($tappayDAO->contribution_recur_id, 'Positive'));
-    $newestExpiryDate = CRM_Core_DAO::singleValueQuery("SELECT MAX(expiry_date) FROM civicrm_contribution_tappay WHERE contribution_recur_id = %1 GROUP BY contribution_recur_id", $params);
+    if (!empty($tappayDAO->contribution_recur_id)) {
+      $params = array( 1 => array($tappayDAO->contribution_recur_id, 'Positive'));
+      $newestExpiryDate = CRM_Core_DAO::singleValueQuery("SELECT MAX(expiry_date) FROM civicrm_contribution_tappay WHERE contribution_recur_id = %1 GROUP BY contribution_recur_id", $params);
+    }
+    else {
+      $newestExpiryDate = $tappayDAO->expiry_date;
+    }
     $returnData[ts('Card Expiry Date')] = date('Y/m',strtotime($newestExpiryDate)).$updateCardmetaButton;
     $returnData[ts('Response Code')] = $tappayObject->status;
     $returnData[ts('Response Message')] = $tappayObject->msg;
