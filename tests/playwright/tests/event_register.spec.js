efba035aa0a688e5ae1a2badf4e44f92a8677acb
chiachin <chiachin@netivism.com.tw>
2022-12-22 18:23:48 +0800

    Refs #36491, Add test to sign up no limit participants and need approval event.

--- tests/playwright/tests/event_register.spec.js
+++ tests/playwright/tests/event_register.spec.js
@@ -3,6 +3,19 @@ const { test, expect, chromium } = require('@playwright/test');
 /** @type {import('@playwright/test').Page} */
 let page;
 
+var item = {
+  event_name_1: '無名額限制，填表完成送出',
+  event_name_2: '有名額限制，不開放候補',
+  event_name_3: '有名額限制，開放候補',
+  event_name_4: '有名額限制，需事先審核',
+  event_name_5: '無名額限制，需事先審核',
+  site_name: 'netiCRM'
+}
+
+function getPageTitle(title){
+  return title + " | "+item.site_name;
+}
+
 test.beforeAll(async () => {
   const browser = await chromium.launch({"headless": false});
   page = await browser.newPage();
@@ -41,12 +54,23 @@ test.describe.serial('Event register page test', () => {
     await expect(page).toHaveTitle('有名額限制，開放候補 | netiCRM');
   });
 
-  test('limit participants. Need approval..', async () => {
+  test('limit participants. Need approval.', async () => {
     await page.goto('/civicrm/event/register?reset=1&action=preview&id=4&cid=0');
     await page.locator('input[name="email-5"]').click();
     await page.locator('input[name="email-5"]').fill('test@aipvo.com');
     await page.locator('text=/.*Continue \\>\\>.*/').click();
     await expect(page).toHaveTitle('有名額限制，需事先審核 | netiCRM');
+  });
+
+  test('No limit participants. Need approval.', async () => {
+    await page.goto('/civicrm/event/register?reset=1&action=preview&id=5&cid=0');
+    await page.locator('input[name="email-5"]').click();
+    await page.locator('input[name="email-5"]').fill('test@aipvo.com');
+    await page.locator('text=/.*Continue \\>\\>.*/').click();
+    var page_title = getPageTitle(item.event_name_5);
+    await expect(page).toHaveTitle(page_title);
+    await expect(page).toHaveURL(/_qf_ThankYou_display/);
+    await expect(page.locator('.bold')).toContainText([/審核|reviewed/i]);
   });
 });
 
