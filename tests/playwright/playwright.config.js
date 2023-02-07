532ddabd6d0e5454805606a8eecb2e98fad32aab
chiachin <chiachin@netivism.com.tw>
2023-02-07 11:43:59 +0800

    Refs #36491, Update config file and event file format.

--- tests/playwright/playwright.config.js
+++ tests/playwright/playwright.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 const { devices } = require('@playwright/test');
 
+
 // Read from default ".env" file.
 require('dotenv').config();
 const path = require('path')
@@ -31,7 +32,9 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+
+  // reporter: [['list', { printSteps: true }]],
+  reporter: './test-reporter.js',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -42,7 +45,7 @@ const config = {
     baseURL: process.env.isLocal === 'true' ? process.env.localUrl : process.env.remoteUrl,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/playwright/tests/event_register.spec.js
+++ tests/playwright/tests/event_register.spec.js
@@ -29,46 +29,67 @@ test.afterAll(async () => {
 test.describe.serial('Event register page test', () => {
   test.use({ storageState: 'storageState.json' });
   test('Normal registration', async () => {
+    var page_title = getPageTitle(item.event_name_1);
     await page.goto('/civicrm/event/register?reset=1&action=preview&id=1&cid=0');
+    await expect(page).toHaveTitle(page_title);
     await page.locator('input[name="email-5"]').click();
     await page.locator('input[name="email-5"]').fill('test@aipvo.com');
     await page.locator('text=/.*Continue \\>\\>.*/').click();
-    await expect(page).toHaveTitle('無名額限制，填表完成送出 | netiCRM');
+    await expect(page).toHaveURL(/_qf_ThankYou_display/);
   });
 
   test.describe('limit participants. Not fot waiting', () => {
     test('Check can register', async () => {
+      var page_title = getPageTitle(item.event_name_2);
       await page.goto('/civicrm/event/register?reset=1&action=preview&id=2&cid=0');
+      await expect(page).toHaveTitle(page_title);
       await page.locator('input[name="email-5"]').click();
       await page.locator('input[name="email-5"]').fill('test@aipvo.com');
       await page.locator('text=/.*Continue \\>\\>.*/').click();
-      await expect(page).toHaveTitle('有名額限制，不開放候補 | netiCRM');
+      await expect(page).toHaveURL(/_qf_ThankYou_display/);
     });
   });
 
-  test('Event register page test :limit participants. Not fot waiting.', async () => {
+  test('limit participants. Not fot waiting.', async () => {
+    var page_title = getPageTitle(item.event_name_3);
     await page.goto('/civicrm/event/register?reset=1&action=preview&id=3&cid=0');
+    await expect(page).toHaveTitle(page_title);
     await page.locator('input[name="email-5"]').click();
     await page.locator('input[name="email-5"]').fill('test@aipvo.com');
     await page.locator('text=/.*Continue \\>\\>.*/').click();
-    await expect(page).toHaveTitle('有名額限制，開放候補 | netiCRM');
+    await expect(page).toHaveURL(/_qf_ThankYou_display/);
   });
 
   test('limit participants. Need approval.', async () => {
+    var page_title = getPageTitle(item.event_name_4);
     await page.goto('/civicrm/event/register?reset=1&action=preview&id=4&cid=0');
+    await expect(page).toHaveTitle(page_title);
     await page.locator('input[name="email-5"]').click();
     await page.locator('input[name="email-5"]').fill('test@aipvo.com');
     await page.locator('text=/.*Continue \\>\\>.*/').click();
-    await expect(page).toHaveTitle('有名額限制，需事先審核 | netiCRM');
+    await expect(page).toHaveURL(/_qf_ThankYou_display/);
   });
 
   test('No limit participants. Need approval.', async () => {
+    var page_title = getPageTitle(item.event_name_5);
     await page.goto('/civicrm/event/register?reset=1&action=preview&id=5&cid=0');
+    
+    //Verity login to event page.
+    await expect(page).toHaveTitle(page_title);
     await page.locator('input[name="email-5"]').click();
     await page.locator('input[name="email-5"]').fill('test@aipvo.com');
     await page.locator('text=/.*Continue \\>\\>.*/').click();
-    var page_title = getPageTitle(item.event_name_5);
+    await expect(page).toHaveURL(/_qf_ThankYou_display/);
+
+    //Verify register successful
+    await expect(page.locator('.bold')).toContainText([/審核|reviewed/i]);
+    await page.goto('/civicrm/event/register?reset=1&action=preview&id=5&cid=0');
+
+    //Second register
     await expect(page).toHaveTitle(page_title);
+    await page.locator('input[name="email-5"]').click();
+    await page.locator('input[name="email-5"]').fill('test2@aipvo.com');
+    await page.locator('text=/.*Continue \\>\\>.*/').click();
     await expect(page).toHaveURL(/_qf_ThankYou_display/);
     await expect(page.locator('.bold')).toContainText([/審核|reviewed/i]);
   });
