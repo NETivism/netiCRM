1b6537f77071edf6812266a24962904fef92149f
chiachin <chiachin@netivism.com.tw>
2023-02-13 15:07:17 +0800

    Refs #34983, Update config and env sample file.

--- tests/playwright/playwright.config.js
+++ tests/playwright/playwright.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 const { devices } = require('@playwright/test');
 
-
 // Read from default ".env" file.
 require('dotenv').config();
 const path = require('path')
@@ -14,13 +13,8 @@ require('dotenv').config({ path: path.resolve(__dirname, 'setup.env') });
 const config = {
   globalSetup: require.resolve('./global-setup'),
   testDir: './tests',
-  /* Maximum time one test can run for. */
   timeout: 120 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
     timeout: 5000
   },
   /* Run tests in files in parallel */
@@ -31,20 +25,17 @@ const config = {
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  // reporter: [['list', { printSteps: true }]],
-  reporter: './test-reporter.js',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html', { open: 'never'}],
+    ['./test-reporter.js']
+  ],
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    headless: true,
     actionTimeout: 10000,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
     storageState: 'storageState.json',
     baseURL: process.env.isLocal === 'true' ? process.env.localUrl : process.env.remoteUrl,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
   },
 
