89ffca6a6f9b284ba282296ebbe0e6a4f187569d
Jimmy Huang <jimmy@netivism.com.tw>
2023-05-01 14:17:03 +0800

    refs gh-70, correct non-static function of DB_Table::throwError

--- packages/DB/Table/Manager/ibase.php
+++ packages/DB/Table/Manager/ibase.php
@@ -374,7 +374,7 @@ class DB_Table_Manager_ibase {
             case 'change':
                 break;
             default:
-                return DB_Table::throwError(DB_TABLE_ERR_ALTER_TABLE_IMPOS);
+                return DB_Table::throwDBError(DB_TABLE_ERR_ALTER_TABLE_IMPOS);
             }
         }
 
--- packages/DB/Table/Manager/oci8.php
+++ packages/DB/Table/Manager/oci8.php
@@ -357,7 +357,7 @@ class DB_Table_Manager_oci8 {
             case 'rename':
                 break;
             default:
-                return DB_Table::throwError(DB_TABLE_ERR_ALTER_TABLE_IMPOS);
+                return DB_Table::throwDBError(DB_TABLE_ERR_ALTER_TABLE_IMPOS);
             }
         }
 
