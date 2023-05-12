fd4b0c6e71fa5d7aaca113cab3ea5e6d7f7cb7d4
poliphilochu <poliphilo@netivism.com.tw>
2023-05-12 18:20:04 +0800

    refs #37771, gh-69, move host trust settings from misc to security

--- CRM/Admin/Form/Setting/Security.php
+++ CRM/Admin/Form/Setting/Security.php
@@ -11,6 +11,17 @@ class CRM_Admin_Form_Setting_Security extends CRM_Admin_Form_Setting {
   public function buildQuickForm() {
     CRM_Utils_System::setTitle(ts('Settings - Website Security'));
 
+    if (CRM_Core_Permission::check('administer neticrm')) {
+      $this->assign('admin', TRUE);
+
+      $this->addTextarea('trustedHostsPatterns', ts('Trusted Host Settings'), array(
+        'placeholder' => ts('Example'). ":" . $_SERVER['HTTP_HOST']
+      ));
+    }
+    else {
+      $this->assign('admin', FALSE);
+    }
+
     parent::buildQuickForm();
   }
 }
