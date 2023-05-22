56da43e7bba4040cd438454df9b828253b017bfc
poliphilochu <poliphilo@netivism.com.tw>
2023-05-22 23:27:15 +0800

    refs #37626. Adjust the structure of AICompletion.js

--- packages/AICompletion/AICompletion.js
+++ packages/AICompletion/AICompletion.js
@@ -1,16 +1,21 @@
 (function($) {
   'use strict';
 
+  // Endpoint
+  var endpoint = {
+    getTemplateList: '/api/getTemplateList',
+    getDefaultTemplate: '/api/getDefaultTemplate',
+    getTemplate: '/api/getTemplate',
+    setTemplate: '/api/setTemplate',
+    getOrganizationIntro: '/api/getOrganizationIntro',
+    setOrganizationIntro: '/api/setOrganizationIntro',
+    chat: '/api/chat',
+    quota: '/api/quota',
+    setShare: '/api/setShare'
+  }
+
   // Default configuration options
-  // TODO: Change to the actual Endpoint later
-  var defaultOptions = {
-    defaultPromptEndpoint: '/api/getDefaultPrompt',
-    sendPromptEndpoint: '/api/sendPrompt',
-    savePromptEndpoint: '/api/saveTemplate',
-    recommendPromptEndpoint: '/api/recommendPrompt',
-    quotaEndpoint: '/api/getQuota',
-    quotaLimit: 10
-  };
+  var defaultOptions = {};
 
   // Plugin constructor function
   $.fn.AICompletion = function(options) {
@@ -39,49 +44,58 @@
         });
       }
 
-      // Load default Prompt
-      function loadDefaultPrompt() {
-        sendAjaxRequest(settings.defaultPromptEndpoint, 'POST', null, function(response) {
-          // Fill the context settings area with default Prompt information
+      // Get default Template
+      function getDefaultTemplate() {
+        sendAjaxRequest(endpoint.getDefaultTemplate, 'POST', null, function(response) {
+          // Fill the context settings area with default Template information
           // TODO: Process and fill the data according to the API response
         });
       }
 
-      // Show more Prompts in a modal dialog
-      function showMorePrompt() {
+      // Apply a Template
+      function applyTemplate(templateData) {
+        // TODO: Apply the template data to the form in the context settings area
+      }
+
+      // Show all templates in a modal dialog
+      function showAllTemplates() {
         // TODO: Create a modal dialog and related elements based on the functional requirements
       }
 
-      // Apply a Prompt
-      function applyPrompt(promptData) {
-        // TODO: Apply the Prompt data to the form in the context settings area
+      // Get template list
+      function getTemplateList(page) {
+        sendAjaxRequest(endpoint.getTemplateList, 'GET', { page: page }, function(response) {
+          // TODO: Process the list of templates according to the data returned by the API
+        });
       }
 
-      // Load organization introduction text
-      function loadOrganizationIntro() {
-        sendAjaxRequest('/api/getOrganizationIntro', 'POST', null, function(response) {
+      // Get organization introduction text
+      function getOrganizationIntro() {
+        sendAjaxRequest(endpoint.getOrganizationIntro, 'POST', null, function(response) {
           // Fill the content summary text area with the organization introduction
           // TODO: Process and fill the data according to the API response
         });
       }
 
-      // Edit organization introduction text
-      function editOrganizationIntro() {
+      // Set organization introduction text
+      function setOrganizationIntro() {
         // TODO: Create an edit modal dialog and related elements based on the functional requirements
+        var data = {};
+
+        sendAjaxRequest(endpoint.setOrganizationIntro, 'POST', data, function(response) {
+        });
       }
 
-      // Send context content
+      // Send prompt content
       function sendPrompt() {
-        var endpoint = settings.sendPromptEndpoint;
-
-        // Get information from the context settings area
+        var data = {};
         var toneStyle = ''; // TODO: Get the tone/style value from the dropdown menu
         var role = ''; // TODO: Get the role value from the dropdown menu
         var summary = ''; // TODO: Get the content summary value from the text area
 
         // TODO: Conditions must be combined into a prompt string
 
-        sendAjaxRequest(endpoint, 'POST', data, function(response) {
+        sendAjaxRequest(endpoint.chat, 'POST', data, function(response) {
           // Display the response result in the result presentation area
           // TODO: Process and display the result based on the API response
         });
@@ -92,23 +106,27 @@
         // TODO: Display the AI response in the result presentation area
       }
 
-      // Recommend Prompts to other organizations
-      function recommendPrompt() {
-        // TODO: Create a modal dialog for recommending Prompts based on the functional requirements
+      // Recommend Template to other organizations
+      function recommendTemplate() {
+        // TODO: Create a modal dialog for recommending Template based on the functional requirements
+        var data = {};
+
+        sendAjaxRequest(endpoint.setShare, 'POST', data, function(response) {
+          // Display the response result in the result presentation area
+          // TODO: Process and display the result based on the API response
+        });
       }
 
-      // Save a Prompt
-      function savePrompt() {
-        // TODO: Create a modal dialog for saving a Prompt based on the functional requirements
+      // Save a template
+      function setTemplate() {
+        // TODO: Create a modal dialog for saving a Template based on the functional requirements
       }
 
       // Check quota usage
       function checkQuotaUsage() {
-        var quotaEndpoint = settings.quotaEndpoint;
-        var quotaLimit = settings.quotaLimit;
-
-        sendAjaxRequest(quotaEndpoint, 'POST', null, function(response) {
-          var quotaUsed = response.quotaUsed;
+        sendAjaxRequest(endpoint.quota, 'POST', null, function(response) {
+          var quotaLimit = response.quotaLimit,
+              quotaUsed = response.quotaUsed;
           // TODO: Retrieve quota usage information from the API response and return the corresponding message
         });
       }
