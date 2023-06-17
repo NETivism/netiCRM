fab5729633558dcf4e4f79b5a55ab0eaf9263905
Jimmy Huang <jimmy@netivism.com.tw>
2023-06-17 12:56:49 +0800

    refs #37904, change CiviUnitTestCase class extend logic

---
+++ tests/phpunit/CiviTest/CiviUnitTestBase.7.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *  PHP 7 version Base class for CiviCRM unit tests
+ *
+ *  Common functions for unit tests
+ *  @package CiviCRM
+ */
+class CiviUnitTestBase extends \PHPUnit\Framework\TestCase {
+}
