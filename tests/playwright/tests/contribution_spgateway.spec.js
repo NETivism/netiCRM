908be31a30fb2addd77c93dc0becb50a4120d88b
maninhouse <maninhouse@protonmail.com>
2023-06-30 17:47:23 +0800

    refs #38169, add playwright test for recurring contribution using payment SPGATEWAY

---
+++ tests/playwright/tests/contribution_spgateway.spec.js
@@ -0,0 +1,152 @@
+const { test, expect, chromium } = require('@playwright/test');
+const utils = require('./utils.js');
+
+/** @type {import('@playwright/test').Page} */
+let page;
+let element;
+
+var vars = {
+    path: 'civicrm/contribute/transact',
+    query: 'reset=1&action=preview&id=3',
+    site_name: 'netiCRM',
+
+    // you should add your own testing variables below
+    page_title: '捐款贊助',
+    user_email: 'youremail@test.tw',
+    first_name: utils.makeid(3),
+    last_name: utils.makeid(3),
+    amount: '101',
+    NewebPay_title: '藍新金流Newebpay第三方支付金流平台',
+};
+
+
+test.beforeAll(async () => {
+    const browser = await chromium.launch();
+    page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+    await page.close();
+});
+
+
+test.describe.serial('SPGATEWAY', () => {
+
+    test.use({ storageState: 'storageState.json' });
+
+    test('Payment', async () => {
+
+        await test.step('Contribution page', async () => {
+
+            /* Step 1: Contribution page */
+            await page.goto('/user/logout');
+            await page.goto(`${vars.path}?${vars.query}`);
+            const page_title = `${vars.page_title} | ${vars.site_name}`;
+            await expect(page).toHaveTitle(page_title);
+            await utils.findElement(page, 'form#Main');
+            await utils.findElement(page, 'div.crm-contribution-main-form-block');
+
+            /* choose "Recurring contributions - monthly" */
+            element = '#CIVICRM_QFID_1_8';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element));
+            await expect(page.locator(element)).toBeChecked();
+
+            /* fill in "Amount" */
+            element = 'input[name="amount_other"]';
+            await utils.findElement(page, element);
+            await utils.fillInput(page.locator(element), vars.amount);
+
+            /* choose "SPGATEWAY" as the "Payment Method" */
+            element = '#CIVICRM_QFID_8_2';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element));
+            await expect(page.locator(element)).toBeChecked();
+
+            /* fill in "Email Address" */
+            element = 'input[name="email-5"]';
+            await utils.findElement(page, element);
+            await utils.fillInput(page.locator(element), vars.user_email);
+
+            /* fill in "First Name" */
+            element = 'input[name="first_name"]';
+            await utils.findElement(page, element);
+            await utils.fillInput(page.locator(element), vars.first_name);
+
+            /* fill in "Last Name" */
+            element = 'input[name="last_name"]';
+            await utils.findElement(page, element);
+            await utils.fillInput(page.locator(element), vars.last_name);
+
+            /* choose No receipt required */
+            element = '#CIVICRM_QFID_0_10';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element));
+            await expect(page.locator(element)).toBeChecked();
+
+            /* choose "Part of Name" */
+            element = '#r_name_half';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element));
+            await expect(page.locator(element)).toBeChecked();
+            
+            /* click "Next >>" */
+            element = '#_qf_Main_upload-bottom';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element), {notExist: '.crm-error'});
+
+        });
+
+        await test.step('Contribution Confirm', async () => {
+
+            /* Step 2: Contribution Confirm */
+            await page.waitForURL(/_qf_Confirm_display/);
+            await expect(page).toHaveURL(/_qf_Confirm_display=true/);
+
+            element = '.amount_display-group .display-block strong';
+            await utils.findElement(page, element);
+            await expect(page.locator(element).first()).toHaveText(`$ ${vars.amount}.00`);
+
+            element = '.contributor_email-section .content';
+            await utils.findElement(page, element);
+            await expect(page.locator(element)).toHaveText(vars.user_email);
+
+            element = '.first_name-section .content';
+            await utils.findElement(page, element);
+            await expect(page.locator(element)).toHaveText(vars.first_name);
+
+            element = '.last_name-section .content';
+            await utils.findElement(page, element);
+            await expect(page.locator(element)).toHaveText(vars.last_name);
+
+            element = '#_qf_Confirm_next-bottom';
+            await utils.findElement(page, element);
+            await utils.clickElement(page, page.locator(element), {notExist: '.crm-error'});
+
+        });
+
+        await test.step('NewebPay Payment Page Check', async () => {
+
+            /* Step 3: NewebPay Payment Page Check */
+            await page.waitForURL('https://ccore.newebpay.com/MPG/period');
+            await expect(page).toHaveURL('https://ccore.newebpay.com/MPG/period');
+        
+        });
+
+        await test.step('Re-login', async () => {
+
+            /* Step 4: Re-login */
+            await page.goto('/');
+            await page.locator('input[name="name"]').fill(process.env.adminUser);
+            await page.locator('input[name="pass"]').fill(process.env.adminPwd);
+            await page.locator('input[value="Log in"]').click();
+
+            // Save signed-in state to 'storageState.json'.
+            await page.context().storageState({ path: 'storageState.json' });
+            await expect(page).toHaveTitle(/Welcome[^|]+ \| netiCRM/);
+
+        });
+
+    });
+
+});
