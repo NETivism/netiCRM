57f5e5109f133eb1be396f93f9f0d755abd84e83
Chang Shu-Huai <junsuwhy@netivism.com.tw>
2023-07-06 12:47:29 +0800

    Refs #37730, Change the echo statement to display the response data in a different format.

--- CRM/AI/CompletionService/OpenAI.php
+++ CRM/AI/CompletionService/OpenAI.php
@@ -167,7 +167,7 @@ class CRM_AI_CompletionService_OpenAI extends CRM_AI_CompletionService {
             }
             if (!empty($decoded["choices"][0]["finish_reason"]) && $decoded["choices"][0]["finish_reason"] === 'stop') {
               $responseData['is_finished'] = 1;
-              echo 'data: [DONE]'.json_encode($responseData)."\n\n";
+              echo 'data: '.json_encode($responseData)."\n\n";
             }
             if (trim($data) === 'data: [DONE]') {
               echo 'data: [DONE]'."\n\n";
