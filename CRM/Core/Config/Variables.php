e3d1f75bc6e901fcf5e99edada016751d775f935
Chen-Sheng Lin <harry@netivism.com.tw>
2023-07-12 14:34:41 +0800

    refs #37730, Add AI copywriting assistant management page.

--- CRM/Core/Config/Variables.php
+++ CRM/Core/Config/Variables.php
@@ -428,6 +428,11 @@ class CRM_Core_Config_Variables extends CRM_Core_Config_Defaults {
   public $wkhtmltopdfPath = FALSE;
 
   /**
+   *  AI Organization profile
+   */
+  public $aiOrganizationProfile = '';
+
+  /**
    * Provide addressSequence
    *
    * @param
