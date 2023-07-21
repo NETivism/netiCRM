ca99dff2d76d893ca088e214ec3bd37e3b3941d5
chiachin <chiachin@netivism.com.tw>
2023-07-21 17:29:17 +0800

    Refs #38118, Updated ci yml to generate reports in HTML format.

--- tests/playwright/playwright.config.js
+++ tests/playwright/playwright.config.js
@@ -28,9 +28,9 @@ const config = {
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    //['list']
+    ['list'],
     ['html', { open: 'never'}],
-    ['./test-reporter.js']
+    // ['./test-reporter.js']
   ],
   use: {
     headless: true,
