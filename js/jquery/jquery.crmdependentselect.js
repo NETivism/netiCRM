10c1d01a14c67d0e1cb50fe678a232f449890996
poliphilochu <poliphilo@netivism.com.tw>
2023-08-02 18:11:00 +0800

    refs #38445. Refactor jquery.crmDependentSelect to dynamically add or remove options for better mobile compatibility.

--- js/jquery/jquery.crmdependentselect.js
+++ js/jquery/jquery.crmdependentselect.js
@@ -1,32 +1,40 @@
 (function($) {
-$.fn.crmDependentSelect = function() {
-  $(".crm-container").on("focus", "select:not(.depended)", function() {
-    $(this).addClass("depended");
-
+  // TODO: If default values are set, dependencies might have issues
+  // TODO: Auto-fill features in some browsers or plugins might set dropdown values before JS execution, possibly causing field misalignment
+  $.fn.crmDependentSelect = function() {
     $(".crm-container select[data-parent]").each(function() {
-      $(this).addClass("depended");
       var $child = $(this),
           parentId = $(this).attr("data-parent"),
-          isParentCustom = $(this).attr("data-parent-custom");
+          isParentCustom = $(this).attr("data-parent-custom"),
+          originalOptions = $child.find('option').clone(); // Store original options
+
+      $(this).addClass("depended");
 
       if (parentId) {
         parentId = isParentCustom != 0 ? "custom_" + parentId : parentId;
         var $parent = $(".crm-container select[name^=" + parentId + "]"),
             parentVal = $parent.val();
 
-        $child.find("option:not([data-parent-filter='" + parentVal + "']").hide();
-        $child.find("option[value='']").show();
-
-        if ($parent.length) {
-          $parent.change(function() {
-            parentVal = $(this).val();
-            $child.find("option").show();
-            $child.find("option[value='']").prop("selected", true);
-            $child.find("option:not([data-parent-filter='" + parentVal + "']").hide();
-          });
+        // Update child options based on parent's value
+        function updateChildOptions(parentVal) {
+          $child.empty().append(originalOptions.clone().filter(function(index, option) {
+            return !$(option).attr('data-parent-filter') || $(option).attr('data-parent-filter') === parentVal;
+          }));
         }
+
+        // Initialize child menu
+        updateChildOptions(parentVal);
+
+        // Update child menu when parent changes
+        $parent.change(function() {
+          updateChildOptions($(this).val());
+        });
       }
     });
-  });
-}
+
+    // TODO: This code snippet might not be necessary; check later
+    $(".crm-container").on("focus", "select:not(.depended)", function() {
+      $(this).addClass("depended");
+    });
+  }
 })(jQuery);
