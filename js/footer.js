06074427f2c63f76ab879c025cdc2fbd00dbd03f
poliphilochu <poliphilo@netivism.com.tw>
2023-12-01 09:46:15 +0800

    refs #37300, gh-90, fix UI issue

--- js/footer.js
+++ js/footer.js
@@ -2,6 +2,15 @@
 $(document).ready(function(){
   let urlParams = new URLSearchParams(window.location.search);
 
+  /**
+   * This is a straightforward JavaScript workaround when 'templates/CRM/common/Embed.tpl' is not used.
+   * Advantages:
+   * - 1. Ensures consistent embedded content styles, especially in scenarios with different templates.
+   * - 2. Simple and convenient to use.
+   * Disadvantages:
+   * - 1. Loads the complete HTML.
+   * - 2. Sometimes might not be the best practice.
+   */
   if (urlParams.get('embed') === '1' && !$('.embed-preview-mode').length) {
     let crmContainerHtml = $('.crm-container').clone().wrap('<div>').parent().html();
     $('body').html(crmContainerHtml).addClass('embed-preview-mode');
