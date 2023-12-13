b7676a5fa834c3ecf01a2db13053dc3d3eb15668
poliphilochu <poliphilo@netivism.com.tw>
2023-12-13 16:40:23 +0800

    refs #37300, add pageTitle in embed mode

--- js/footer.js
+++ js/footer.js
@@ -13,7 +13,15 @@ $(document).ready(function(){
    */
   if (urlParams.get('embed') === '1' && !$('.embed-preview-mode').length) {
     let crmContainerHtml = $('.crm-container').clone().wrap('<div>').parent().html();
+    let pageTitle = $('.page-title').length ? $('.page-title').text().trim() : $('title').text().split('|')[0].trim();
+
     $('body').html(crmContainerHtml).addClass('embed-preview-mode');
+
+    if (pageTitle) {
+      let pageTitleHTML = `<div class="preview-page-title-block"><div class="inner"><h1 class="page-title">${pageTitle}</h1></div></div>`;
+
+      $('.crm-container').prepend(pageTitleHTML);
+    }
   }
 
   if (document.querySelector('.crm-container select[data-parent]')) {
