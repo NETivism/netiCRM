fd3d2997924bb40a590df43f83e99afa6bd7636b
Chloe <chloe60492@gmail.com>
2024-02-16 15:21:53 +0800

    ref #39808 modify event testing file for limit participants and no waitlist

--- tests/playwright/tests/event_limit_noWait_register.spec.js
+++ tests/playwright/tests/event_limit_noWait_register.spec.js
@@ -9,6 +9,21 @@ const wait_secs = 2000;
 test.beforeAll(async () => {
   const browser = await chromium.launch();
   page = await browser.newPage();
+  await test.step('Check the participant number is correct', async()=>{
+    const response = await page.goto('/civicrm/event/search?reset=1&force=1&event=2');
+    await expect(response.status()).toBe(200);
+    await expect(page).toHaveTitle('有名額限制，不開放候補 | netiCRM');
+    const counted_people = await page.locator('div#stat_ps>div#stat_ps_label1>ol>li>div>span.people-count').first().textContent();
+    console.log('counted people:', counted_people);
+    // check the number of participants
+    if (counted_people == '5'){
+      // capture the fifth participant and delete it
+      await page.locator('div#participantSearch>table>tbody>tr').first().getByRole('link', { name: 'Delete' }).click();
+      await page.locator('[id="_qf_Participant_next-bottom"]').click();
+    }
+    // check whether it was deleted sucessfully
+    await expect(page.locator('div#stat_ps>div#stat_ps_label1>ol>li>div>span.people-count').first()).toHaveText('4');
+  })
 });
 
 test.afterAll(async () => {
@@ -18,22 +33,7 @@ test.afterAll(async () => {
 
 
 test('limit participants. Not for waiting', async() => {
-  await test.step('Delete participants', async()=>{
-    const response = await page.goto('/civicrm/event/search?reset=1&force=1&event=2');
-    await expect(response.status()).toBe(200);
-    const before_people = await page.locator('div#stat_ps>div#stat_ps_label1>ol>li>div>span.people-count').first().textContent();
-    console.log('before people:', before_people);
-    // check the number of participants
-    if (before_people == '5'){
-      // capture the newest one
-      // const newest_element = await page.locator('div#participantSearch>table>tbody>tr').first();
-      // console.log('the newest one locator is:', newest_element);
-      await page.locator('div#participantSearch>table>tbody>tr').first().getByRole('link', { name: 'Delete' }).click();
-      await page.locator('[id="_qf_Participant_next-bottom"]').click();
-    }
-    // check whether delete sucessfully
-    await expect(page.locator('div#stat_ps>div#stat_ps_label1>ol>li>div>span.people-count').first()).toHaveText('4');
-  })
+  await page.goto('/user/logout');
   var page_title =  await utils.getPageTitle('有名額限制，不開放候補');
   await test.step('Check can register and second participant message is correct.', async () => {
     await page.goto('/civicrm/event/register?cid=0&reset=1&id=2');
