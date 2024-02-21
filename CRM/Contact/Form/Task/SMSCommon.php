5d07a1e3363bb44c5e62d442a08b6d06b4717cd9
Jimmy Huang <jimmy@netivism.com.tw>
2024-02-21 15:36:30 +0800

    refs #39974, fixes sms should respect is_primary mobile phone

--- CRM/Contact/Form/Task/SMSCommon.php
+++ CRM/Contact/Form/Task/SMSCommon.php
@@ -102,7 +102,7 @@ class CRM_Contact_Form_Task_SMSCommon {
 SELECT civicrm_phone.contact_id, civicrm_phone.phone, civicrm_phone.id as phone_id, civicrm_phone.phone_type_id
 FROM civicrm_contact
 LEFT JOIN civicrm_phone ON ( civicrm_contact.id = civicrm_phone.contact_id )
-WHERE civicrm_contact.id IN (%1) AND civicrm_phone.phone_type_id = %2 GROUP BY civicrm_phone.contact_id
+WHERE civicrm_contact.id IN (%1) AND civicrm_phone.phone_type_id = %2
 ORDER BY civicrm_phone.is_primary DESC, phone_id ASC";
       $mobilePhoneResult = CRM_Core_DAO::executeQuery($mobilePhoneQuery, array(
         1 => array(CRM_Utils_Array::implode(',', $form->_contactIds), 'CommaSeperatedIntegers'),
@@ -111,9 +111,11 @@ ORDER BY civicrm_phone.is_primary DESC, phone_id ASC";
       while ($mobilePhoneResult->fetch()) {
         if (!empty(trim($mobilePhoneResult->phone))) {
           $contactId = $mobilePhoneResult->contact_id;
-          $form->_contactDetails[$contactId]['phone_id'] = $mobilePhoneResult->phone_id;
-          $form->_contactDetails[$contactId]['phone'] = trim($mobilePhoneResult->phone);
-          $form->_contactDetails[$contactId]['phone_type_id'] = $mobilePhoneResult->phone_type_id;
+          if (!isset($form->_contactDetails[$contactId]['phone_id'])) {
+            $form->_contactDetails[$contactId]['phone_id'] = $mobilePhoneResult->phone_id;
+            $form->_contactDetails[$contactId]['phone'] = trim($mobilePhoneResult->phone);
+            $form->_contactDetails[$contactId]['phone_type_id'] = $mobilePhoneResult->phone_type_id;
+          }
         }
       }
 
