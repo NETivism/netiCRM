13fff768246752fc95ff2f87ef32a04539c3af4b
Chloe <chloe60492@gmail.com>
2024-03-21 10:56:37 +0800

    ref #40044, complete add new membership testing

--- tests/playwright/tests/add_membership_type.spec.js
+++ tests/playwright/tests/add_membership_type.spec.js
@@ -17,7 +17,8 @@ test.afterAll(async () => {
 
 test.describe.serial('Create Membership Type', () => {
     var organization = utils.makeid(10);
-    var membership_type = 'typeForTest';
+    var membership_type = utils.makeid(5);
+    var element;
     test('Create Organization Contact', async () => {
         await page.goto('civicrm/contact/add?reset=1&ct=Organization');
         await utils.wait(wait_secs);
@@ -46,5 +47,37 @@ test.describe.serial('Create Membership Type', () => {
         await page.locator('[id="_qf_MembershipType_upload-bottom"]').click();
         await expect(page.getByRole('cell', { name: membership_type })).toHaveText(membership_type);
     });
+    test('Create Membership', async ()=> {
+        var firstName = 'test_firstName';
+        var lastName = 'test_lastName';
+        var name = firstName + ' ' + lastName;
+        // go to create membership page
+        await page.goto('/civicrm/member/add?reset=1&action=add&context=standalone');
+        
+        // create individual data
+        await page.locator('#profiles_1').selectOption('4');
+        await expect(page.getByRole('dialog')).toBeVisible();
 
+        element = await utils.findElementByLabel(page, '名字\n     *');
+        await utils.fillInput(element, firstName);
+        element = await utils.findElementByLabel(page, '姓氏\n     *');
+        await utils.fillInput(element, lastName);
+        await page.locator('#_qf_Edit_next').click();
+        await expect(page.locator('#contact_1')).toHaveValue(name);
+
+        // select the first option in the membership type and orginization
+        element = '[id="membership_type_id\\[0\\]"]';
+        await utils.selectOption(page.locator(element), { index: 0 }); 
+        element = '[id="membership_type_id\\[1\\]"]';
+        await utils.selectOption(page.locator(element), { index: 1 }); 
+        // pick the first date
+        await page.locator('#join_date').click();
+        await page.getByRole('link', { name: '1', exact: true }).click();
+        await page.locator('#start_date').click();
+        await page.getByRole('link', { name: '1', exact: true }).click();
+        await page.locator('[id="_qf_Membership_upload-bottom"]').click();
+        await utils.wait(wait_secs);
+        await expect(page).toHaveTitle(name + ' | netiCRM');
+        await expect(page.locator('#option11>tbody')).toContainText(membership_type);
+    });
 });
