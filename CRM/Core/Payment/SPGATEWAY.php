5902b057e96afdcb433a0734bec55dddfec60def
Chang Shu-Huai <junsuwhy@netivism.com.tw>
2024-05-08 11:55:59 +0800

    Refs #39880, #38634, Fixed confused params for spgateway used.

--- CRM/Core/Payment/SPGATEWAY.php
+++ CRM/Core/Payment/SPGATEWAY.php
@@ -266,11 +266,11 @@ class CRM_Core_Payment_SPGATEWAY extends CRM_Core_Payment {
           CRM_Core_DAO::setFieldValue('CRM_Event_DAO_Participant', $params['participantID'], 'status_id', $newStatus, 'id');
           $cancelledStatus = array_search('Cancelled', $participantStatus);
           $sql = 'SELECT id FROM civicrm_participant WHERE registered_by_id = %1 AND status_id != %2';
-          $params = array(
+          $paramsRegisteredBy = array(
             1 => array($params['participantID'], 'Integer'),
             2 => array($cancelledStatus, 'Integer'),
           );
-          $dao = CRM_Core_DAO::executeQuery($sql, $params);
+          $dao = CRM_Core_DAO::executeQuery($sql, $paramsRegisteredBy);
           while($dao->fetch()){
             CRM_Core_DAO::setFieldValue('CRM_Event_DAO_Participant', $dao->id, 'status_id', $newStatus, 'id');
           }
