438ceb9a6662287f7b807189e28a8dc00d1bbb0c
Chang Shu-Huai <junsuwhy@netivism.com.tw>
2024-06-04 14:35:55 +0800

    Refs #40730, Modify checkUserNameEmailExists format in Drupal8.

--- CRM/Utils/System/Drupal8.php
+++ CRM/Utils/System/Drupal8.php
@@ -134,12 +134,12 @@ class CRM_Utils_System_Drupal8 {
    *
    * @param array $params
    *   Array of name and mail values.
-   * @param array $errors
-   *   Errors.
    * @param string $emailName
    *   Field label for the 'email'.
+   * @return @param array $errors
+   *   Errors.
    */
-  public static function checkUserNameEmailExists(&$params, &$errors, $emailName = 'email') {
+  public static function checkUserNameEmailExists(&$params, $emailName = 'email') {
     // If we are given a name, let's check to see if it already exists.
     if (!empty($params['name'])) {
       $name = $params['name'];
@@ -185,6 +185,7 @@ class CRM_Utils_System_Drupal8 {
         $errors[$emailName] = (string) $violations[0]->getMessage();
       }
     }
+    return $errors;
   }
 
   /**
@@ -468,6 +469,9 @@ class CRM_Utils_System_Drupal8 {
     \Drupal::service('civicrm')->initialize();
 
     if ($loadUser) {
+      if (empty($params['uid'])) {
+        $params['uid'] = 0;
+      }
       $userObject = \Drupal\user\Entity\User::load($params['uid']);
       if (!empty($params['uid']) && $username = $userObject->getAccountName()) {
         $this->loadUserByName($username);
