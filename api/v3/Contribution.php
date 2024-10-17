6800f5f46c8f899e63f7a91612f0275abee25e70
Jimmy Huang <jimmy@netivism.com.tw>
2024-10-17 16:46:26 +0800

    refs #41550, fixes contribution api query shouldn't return deleted contact by default

--- api/v3/Contribution.php
+++ api/v3/Contribution.php
@@ -156,6 +156,10 @@ function civicrm_api3_contribution_get($params) {
   $additionalOptions = _civicrm_api3_get_options_from_params($params, TRUE);
   $returnProperties = CRM_Contribute_BAO_Query::defaultReturnProperties($mode);
 
+  // filter out deleted contact by default
+  if (!isset($params['contact_is_deleted'])) {
+    $params['contact_is_deleted'] = 0;
+  }
   // Get the contributions based on parameters passed in
   $contributions = _civicrm_api3_get_using_query_object('Contribution', $params, $additionalOptions, NULL, $mode, $returnProperties, TRUE);
   if (!empty($contributions)) {
