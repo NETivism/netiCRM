3e44740fc0f3f896e4f2b2db4066ebb5940b9420
Jimmy Huang <jimmy@netivism.com.tw>
2025-02-24 13:31:16 +0800

    refs #41568, add require_once back for drupal 7 bootstrap

--- CRM/Utils/System/Drupal7.php
+++ CRM/Utils/System/Drupal7.php
@@ -19,7 +19,7 @@ class CRM_Utils_System_Drupal7 {
       }
       return FALSE;
     }
-
+    require_once "$cmsPath/includes/bootstrap.inc";
     @drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
     // explicitly setting error reporting, since we cannot handle drupal related notices
     // @todo 1 = E_ERROR, but more to the point setting error reporting deep in code
