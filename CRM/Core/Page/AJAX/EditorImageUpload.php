73460a816c93fe2685f64253b862285db1c87f26
poliphilochu <poliphilo@netivism.com.tw>
2025-06-03 16:36:36 +0800

    refs #41707, add editor image upload endpoint

---
+++ CRM/Core/Page/AJAX/EditorImageUpload.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * AJAX handler for editor image upload functionality
+ */
+class CRM_Core_Page_AJAX_EditorImageUpload {
+
+  /**
+   * Handle image upload from editor
+   * 
+   * @return void
+   */
+  public static function upload() {
+    if (!CRM_Core_Permission::check('access CiviCRM')) {
+      self::responseError([
+        'status' => 0,
+        'message' => 'Permission denied. Access CiviCRM permission required.'
+      ]);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+      self::responseError([
+        'status' => 0,
+        'message' => 'Only POST method is allowed.'
+      ]);
+    }
+    
+    self::responseSuccess([
+      'status' => 1,
+      'message' => 'Upload endpoint is working'
+    ]);
+  }
+
+  /**
+   * This function handles the response in case of an error.
+   *
+   * @param mixed $error The error message or object that needs to be sent as a response.
+   */
+  public static function responseError($error) {
+    http_response_code(400);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($error);
+    CRM_Utils_System::civiExit();
+  }
+
+  /**
+   * This function handles the response in case of success.
+   *
+   * @param mixed $data The data that needs to be sent as a response.
+   */
+  public static function responseSuccess($data) {
+    http_response_code(200);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data);
+    CRM_Utils_System::civiExit();
+  }  
+}
