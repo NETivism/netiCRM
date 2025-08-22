f860041fe983e70d9f0f3beb18b52ba0cebfa251
Jimmy Huang <jimmy@netivism.com.tw>
2025-08-22 12:43:43 +0800

    refs #44496, #43167, fixes detect button issue on submit recur form

--- CRM/Contribute/Form/MakingTransaction.php
+++ CRM/Contribute/Form/MakingTransaction.php
@@ -130,13 +130,10 @@ class CRM_Contribute_Form_MakingTransaction extends CRM_Core_Form {
     }
     $recurId = $this->get('recurId');
     $contributionId = $this->get('contributionId');
-
-    if (isset($this->_elementIndex['_qf_MakingTransaction_upload'])) {
-      $isActionUpdate = TRUE;
-    }
-
     $paymentClass = CRM_Contribute_BAO_Contribution::getPaymentClass($contributionId);
-    if ($isActionUpdate) {
+
+    $buttonName = $this->controller->getButtonName();
+    if ($buttonName == '_qf_MakingTransaction_upload') {
       if (method_exists($paymentClass, 'doRecurUpdate')) {
         $resultMessage = $paymentClass::doRecurUpdate($recurId, 'recur', $this);
       }
