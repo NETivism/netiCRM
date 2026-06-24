db6452bc948002c5ebd6adc11122deecdbff85bd
Jimmy Huang <jimmy@netivism.com.tw>
2026-06-24 10:54:10 +0800

    refs #45587, #45597, do not lock pending status, allow mobile have recur payment

--- CRM/Contribute/Form/ContributionPage/Amount.php
+++ CRM/Contribute/Form/ContributionPage/Amount.php
@@ -105,7 +105,7 @@ class CRM_Contribute_Form_ContributionPage_Amount extends CRM_Contribute_Form_Co
 SELECT id
   FROM civicrm_payment_processor
  WHERE id IN ({$paymentProcessorIds})
-   AND is_recur = 1";
+   AND (is_recur = 1 OR payment_processor_type = 'Mobile')";
       $dao = &CRM_Core_DAO::executeQuery($query);
       while ($dao->fetch()) {
         $recurringPaymentProcessor[] = $dao->id;
