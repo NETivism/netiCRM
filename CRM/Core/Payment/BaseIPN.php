8e9e5ad8f167d21c5f094ad055e2d6dde7237424
poliphilo <poliphilo@netivism.com.tw>
2026-08-13 10:03:56 +0800

    refs #43453, feat: add non_deductible_amount option to recurring sync exclude

--- CRM/Core/Payment/BaseIPN.php
+++ CRM/Core/Payment/BaseIPN.php
@@ -894,6 +894,13 @@ class CRM_Core_Payment_BaseIPN {
       if ($config->copyContributionTypeSource == 1) {
         $c->contribution_type_id = CRM_Core_DAO::getFieldValue('CRM_Contribute_DAO_ContributionPage', $c->contribution_page_id, 'contribution_type_id');
       }
+      // Non-deductible amount is copied by default, which is correct for
+      // non-deductible contribution types. Site admins can opt out via the
+      // "Exclude to sync" setting, then the DB default (0) applies.
+      $exclude = !empty($config->recurringSyncExclude) ? $config->recurringSyncExclude : [];
+      if (in_array('non_deductible_amount', $exclude, TRUE)) {
+        unset($c->non_deductible_amount);
+      }
       $transaction = new CRM_Core_Transaction();
       $c->save();
       $transaction->commit();
