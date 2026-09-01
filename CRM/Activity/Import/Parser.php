1f52ec14601fe237f2191aa661e35a170282cc74
Jimmy Huang <jimmy@netivism.com.tw>
2026-09-01 05:12:55 +0000

    refs #45753, (feat) fixes php warning: omitted CSV escape parameters
    Warning detail: Omitting the CSV escape argument is deprecated and its default value will change.
    Fix: Pass the historical backslash escape explicitly to fgetcsv() and fputcsv().

--- CRM/Activity/Import/Parser.php
+++ CRM/Activity/Import/Parser.php
@@ -257,7 +257,7 @@ abstract class CRM_Activity_Import_Parser {
     while (!feof($fd)) {
       $this->_lineCount++;
 
-      $values = fgetcsv($fd, 8192, $seperator);
+      $values = fgetcsv($fd, 8192, $seperator, '"', '\\');
       if (!$values) {
         continue;
       }
